a5f9773543431e71f5f347ad5e41a5f8ca51749e
Evan Turner <evturn@gmail.com>
2017-02-14 22:34:47 -0500

    Add type checking exercise

---
+++ 05/TypeChecking.hs
@@ -0,0 +1,43 @@
+module TypeChecking where
+
+f :: Int -> String
+f = undefined
+
+g :: String -> Char
+g = undefined
+
+h :: Int -> Char
+h a = (g (f a))
+
+
+
+data A
+data B
+data C
+
+q :: A -> B
+q = undefined
+
+w :: B -> C
+w = undefined
+
+e :: A -> C
+e x = (w (q x))
+
+
+
+data X
+data Y
+data Z
+
+xz :: X -> Z
+xz = undefined
+
+yz :: Y -> Z
+yz = undefined
+
+xform :: (X, Y) -> (Z, Z)
+xform (x, y) = (xz x, yz y)
+
+munge :: (x -> y) -> (y -> (w, z)) -> x -> w
+munge xY yWZ x = fst (yWZ (xY x))
