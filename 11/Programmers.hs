dab1970890633d527887c62d7d6ba8d67e178860
Evan Turner <evturn@gmail.com>
2017-03-24 20:12:12 -0500

    Add programmers exercise for chapter 11

---
+++ 11/Programmers.hs
@@ -0,0 +1,38 @@
+
+
+data OperatingSystem =
+       GnuPlusLinux
+     | OpenBSDPlusNevermindJustBSDStill
+     | Mac
+     | Windows
+     deriving (Eq, Show)
+
+data ProgrammingLanguage =
+       Haskell
+     | Agda
+     | Idris
+     | PureScript
+     deriving (Eq, Show)
+
+data Programmer =
+  Programmer { os :: OperatingSystem
+             , lang :: ProgrammingLanguage }
+  deriving (Eq, Show)
+
+nineToFive :: Programmer
+nineToFive = Programmer { os = Mac
+                        , lang = Haskell }
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [ GnuPlusLinux
+  , OpenBSDPlusNevermindJustBSDStill
+  , Mac
+  , Windows
+  ]
+
+allLanguages :: [ProgrammingLanguage]
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers = [ Programmer x y | x <- allOperatingSystems, y <- allLanguages ]
