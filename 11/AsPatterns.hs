b11b6e76138fa7f2180444c044e1de7bc93e2f47
Evan Turner <evturn@gmail.com>
2017-03-26 14:07:12 -0400

    Update AsPatterns

--- 11/AsPatterns.hs
+++ 11/AsPatterns.hs
@@ -1,6 +1,20 @@
+import Data.Char (toUpper)
+import Data.List (intercalate)
+
 isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
 isSubsequenceOf [] _ = True
 isSubsequenceOf _ [] = False
 isSubsequenceOf (x:xs) (y:ys) = case x == y of
   True  -> isSubsequenceOf xs ys
   False -> False
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords xs = map f $ words xs
+  where f word@(y:ys) = (word, toUpper y : ys)
+
+capitalizeWord :: String -> String
+capitalizeWord [] = []
+capitalizeWord (x:xs) = toUpper x : xs
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph xs = concatMap capitalizeWord $ groupBy ((==) `on` (=='.')) xs
