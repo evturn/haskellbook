26230846fc1cfa120b4c69228b975bcf8449ae12
Evan Turner <evturn@gmail.com>
2017-03-26 09:06:49 -0400

    Convert BinaryTree types into lists

--- 11/BinaryTree.hs
+++ 11/BinaryTree.hs
@@ -40,7 +40,36 @@ testTree' =
 
 mapExpected = Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
 
-mapOkay =
-  if mapTree (+1) testTree' == mapExpected
-  then print "Yes things were fine."
-  else error "Test found things to be kinda bad."
+mapOkay = if mapTree (+1) testTree' == mapExpected then print "Yes things were fine." else error "Test found things to be kinda bad."
+
+--------------------------
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = [a] ++ preorder left ++ preorder right
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = inorder left ++ [a] ++ inorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = postorder left ++ postorder right ++ [a]
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder = if preorder testTree == [2, 1, 3] then putStrLn "Preorder was chill." else putStrLn "Preorder not chill."
+
+testInorder :: IO ()
+testInorder = if inorder testTree == [1, 2, 3] then putStrLn "Inorder was totally chill" else putStrLn "Inorder not good."
+
+testPostorder :: IO ()
+testPostorder = if postorder testTree == [1, 3, 2] then putStrLn "Postorder was cool" else putStrLn "Postorder not cool."
+
+main :: IO ()
+main = do
+  testPreorder
+  testInorder
+  testPostorder
