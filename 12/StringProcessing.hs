6514aeb04715984ede8ca42f06643ec828df4d10
Evan Turner <evturn@gmail.com>
2017-04-05 22:58:01 -0500

    Make string processing exercise a module

--- 12/StringProcessing.hs
+++ 12/StringProcessing.hs
@@ -1,5 +1,13 @@
+module StringProcessing where
+
 replaceThe :: String -> String
 replaceThe xs = unwords $ map sayByeToThe (words xs)
   where
     sayByeToThe "the" = "a"
     sayByeToThe x     = x
+
+runTest :: String
+runTest = replaceThe "Man, this chocolate cake is the shit!"
+
+main :: IO ()
+main = print $ runTest
