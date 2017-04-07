9336302ea0a3861ec4a20d6c8356a38645381fda
Evan Turner <evturn@gmail.com>
2017-04-06 22:16:28 -0500

    Add vowel string processing exercise

--- 12/StringProcessing.hs
+++ 12/StringProcessing.hs
@@ -6,8 +6,20 @@ replaceThe xs = unwords $ map sayByeToThe (words xs)
     sayByeToThe "the" = "a"
     sayByeToThe x     = x
 
-runTest :: String
-runTest = replaceThe "Man, this chocolate cake is the shit!"
+testReplaceThe :: String
+testReplaceThe = replaceThe "Man, this chocolate cake is the shit!"
+
+vowels :: [Char]
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel = counter . words
+  where
+    counter (x1:x2:xs)
+      | x1 == "the" && head x2 `elem` vowels = 1 + counter (x2:xs)
+      | otherwise                            = 0 + counter (x2:xs)
+    counter _ = 0
+
 
 main :: IO ()
-main = print $ runTest
+main = print $ countTheBeforeVowel "Where are the oranges I left in the upper cabinet"
