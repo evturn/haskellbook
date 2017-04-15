8b05e989d6cd2f3b3d613ce2c0c43ef4c675c19c
Evan Turner <evturn@gmail.com>
2017-04-14 22:18:19 -0400

    Add third exercise to chap 12

--- 12/MaybeLib.hs
+++ 12/MaybeLib.hs
@@ -16,5 +16,15 @@ isNothing :: Maybe a -> Bool
 isNothing (Just _) = False
 isNothing Nothing = True
 
+
+-- >>> mayybee 0 (+1) Nothing
+-- 0
+-- >>> mayybee 0 (+1) (Just 1)
+-- 2
+
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee b _ Nothing = b
+mayybee b f (Just a) = f a
+
 main :: IO ()
-main = print (isJust (Just 1))
+main = print (mayybee 0 (+1) (Just 1))
