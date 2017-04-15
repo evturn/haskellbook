5893a1649e3a9df07d8aa3301d33d83ff5844e93
Evan Turner <evturn@gmail.com>
2017-04-15 02:12:25 -0400

    Add function taking Either and returning Maybe

--- 12/EitherLib.hs
+++ 12/EitherLib.hs
@@ -15,3 +15,7 @@ partitionEithers' = foldr f ([], [])
   where
     f (Left a) (xs, ys)  = (a:xs, ys)
     f (Right b) (xs, ys) = (xs, b:ys)
+
+eitherMaybe' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe' _ (Left _)  = Nothing
+eitherMaybe' f (Right b) = Just (f b)
