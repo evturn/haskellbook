5a7dcae954e6daa6f5508fa7bd4d6091c4811f2e
Evan Turner <evturn@gmail.com>
2017-04-22 12:05:42 -0400

    Normalize input before checking for palindrome

--- 13/Palindrome.hs
+++ 13/Palindrome.hs
@@ -1,10 +1,17 @@
 import Control.Monad
 import System.Exit (exitSuccess)
+import Data.Char (isAlphaNum, toLower)
+
+normalizeString :: String -> String
+normalizeString = map toLower . filter isAlphaNum
+
+checkSentence :: String -> Bool
+checkSentence s = (normalizeString s) == (reverse $ normalizeString s)
 
 palindrome :: IO ()
 palindrome = forever $ do
   line1 <- getLine
-  if line1 == reverse line1
+  if checkSentence line1
   then do putStrLn "It's a palindrome"
           return ()
   else do putStrLn "Nah"
