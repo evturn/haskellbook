55a236e734401aca9a3463e76f2780ad33961af7
Evan Turner <evturn@gmail.com>
2017-04-23 00:32:11 -0400

    Run hspec in Addition module

--- 14/addition/Addition.hs
+++ 14/addition/Addition.hs
@@ -1,4 +1,12 @@
 module Addition where
 
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
+
 sayHello :: IO ()
 sayHello = putStrLn "No."
