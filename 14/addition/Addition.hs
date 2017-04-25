9ded953766ddcebb22fd5a97c949ea854a4ad7fc
Evan Turner <evturn@gmail.com>
2017-04-24 22:12:36 -0500

    Describe recursive function using hspec

--- 14/addition/Addition.hs
+++ 14/addition/Addition.hs
@@ -5,8 +5,16 @@ import Test.Hspec
 main :: IO ()
 main = hspec $ do
   describe "Addition" $ do
-    it "1 + 1 is greater than 1" $ do
-      (1 + 1) > 1 `shouldBe` True
+    it "15 divided by 3 is 5" $ do
+      dividedBy 15 3 `shouldBe` (5, 0)
+    it "22 divided by 5 is 4 remainder 2" $ do
+      dividedBy 22 5 `shouldBe` (4, 2)
+
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+  where go n   d count
+         | n < d = (count, n)
+         | otherwise = go (n - d) d (count + 1)
 
 sayHello :: IO ()
 sayHello = putStrLn "No."
