e2a1cf7779ced7d96135040770e958be7f25a44f
Evan Turner <evturn@gmail.com>
2017-04-28 02:50:41 -0400

    Return one sum type more often in QuickCheck test

--- 14/Arbitrary.hs
+++ 14/Arbitrary.hs
@@ -59,6 +59,17 @@ sumGenEqual = do
 sumGenCharInt :: Gen (Sum Char Int)
 sumGenCharInt = sumGenEqual
 
+
+sumGenFirstPls :: (Arbitrary a, Arbitrary b) => Gen (Sum a b)
+sumGenFirstPls = do
+  a <- arbitrary
+  b <- arbitrary
+  frequency [(10, return $ First a),
+             (1,  return $ Second b)]
+
+sumGenCharIntFirst :: Gen (Sum Char Int)
+sumGenCharIntFirst = sumGenFirstPls
+
 main :: IO ()
 main = do
   sample trivialGen
