331b7e233e3c4fff446e3dfb83e42d9095fe1487
Evan Turner <evturn@gmail.com>
2017-04-30 14:12:22 -0500

    Create WordNumber tests

---
+++ 14/word-number/src/WordNumber.hs
@@ -0,0 +1,27 @@
+module WordNumber
+     ( digitToWord
+     , digits
+     , wordNumber
+     ) where
+
+import Data.List (intersperse)
+
+digitToWord :: Int -> String
+digitToWord 0 = "zero"
+digitToWord 1 = "one"
+digitToWord 2 = "two"
+digitToWord 3 = "three"
+digitToWord 4 = "four"
+digitToWord 5 = "five"
+digitToWord 6 = "six"
+digitToWord 7 = "seven"
+digitToWord 8 = "eight"
+digitToWord 9 = "nine"
+
+digits :: Int -> [Int]
+digits n
+  | n == 0    = []
+  | otherwise = digits (div n 10) ++ [mod n 10]
+
+wordNumber :: Int -> String
+wordNumber n = concat . intersperse "-" . map digitToWord $ digits n
