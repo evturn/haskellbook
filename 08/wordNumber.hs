8f6057eb115c5597d643bf4cd63c34971636c213
Evan Turner <evturn@gmail.com>
2017-04-29 22:14:16 -0500

    Add WordNumber exercise to chapter 8

--- 08/wordNumber.hs
+++ 08/wordNumber.hs
@@ -3,10 +3,21 @@ module WordNumber where
 import Data.List (intersperse)
 
 digitToWord :: Int -> String
-digitToWord n = undefined
+digitToWord 0 = "zero"
+digitToWord 1 = "one"
+digitToWord 2 = "two"
+digitToWord 3 = "three"
+digitToWord 4 = "four"
+digitToWord 5 = "five"
+digitToWord 6 = "six"
+digitToWord 7 = "seven"
+digitToWord 8 = "eight"
+digitToWord 9 = "nine"
 
 digits :: Int -> [Int]
-digits n = undefined
+digits n
+  | n == 0    = []
+  | otherwise = digits (div n 10) ++ [mod n 10]
 
 wordNumber :: Int -> String
-wordNumber n = undefined
+wordNumber n = concat . intersperse "-" . map digitToWord $ digits n
