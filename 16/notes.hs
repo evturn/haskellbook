0c74a5042b17fd250bcaa618942bf9674e9a76c6
Evan Turner <evturn@gmail.com>
2017-06-13 22:23:22 -0500

    Add notes for chapter 16

---
+++ 16/notes.hs
@@ -0,0 +1,9 @@
+-- Functor is all about a pattern of mapping over structure.
+
+-- Functors are combinators:
+   -- they take a sentence or phrase as input and produce a sentence or phrase as an output, with
+   -- some logical operation applied to the whole.
+
+-- A functor is a way to apply a function over or around some structure that we don’t want to alter.
+   -- That is, we want to apply the function to the value that is “inside” some structure and leave
+   -- the structure alone.
