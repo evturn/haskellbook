4318cc84419ae4f3faf8f2fdcf3a9c8ad73cd043
Evan Turner <evturn@gmail.com>
2017-06-14 22:23:22 -0500

    Add notes on the Functor typeclass declaration

--- 16/notes.hs
+++ 16/notes.hs
@@ -7,3 +7,11 @@
 -- A functor is a way to apply a function over or around some structure that we don’t want to alter.
    -- That is, we want to apply the function to the value that is “inside” some structure and leave
    -- the structure alone.
+
+-- Definition of the Functor typeclass:
+
+class Functor f where
+  fmap :: (a -> b) -> f a -> f b
+
+-- `fmap` takes a function as an argument, a `Functor` f that takes a type argument `a`, then returns
+   -- the same `f` while the type argument `b` possibly but not necessarily refers to a different type.
