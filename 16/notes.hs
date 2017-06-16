5b0b8ef83632741fd2736dfd1a44112cea5a12bb
Evan Turner <evturn@gmail.com>
2017-06-15 22:23:22 -0500

    Describe Functor typeclass and fmap definition

--- 16/notes.hs
+++ 16/notes.hs
@@ -15,3 +15,13 @@ class Functor f where
 
 -- `fmap` takes a function as an argument, a `Functor` f that takes a type argument `a`, then returns
    -- the same `f` while the type argument `b` possibly but not necessarily refers to a different type.
+
+-- Every argument to the type constructor of `->` must be of kind `*`.
+-- Each argument and result of every function must be a type constant, not a type constructor.
+
+class Functor f where
+  fmap :: (a -> b) ->  f a  ->  f b
+-- has kind:  *    ->   *   ->   *
+
+-- The type signature of `fmap` tells us tha the f introduced by the class definition for Functor must
+-- accept a single argument and thus be of kind `* -> *`.
