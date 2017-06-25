c5a1766ef65aabbc46beca9ea33525823e9e55ec
Evan Turner <evturn@gmail.com>
2017-06-25 14:29:12 -0400

    Add parts 2 and 3 of Functor instance chapter exercises

--- 16/chapter-exercises/functor-instance.hs
+++ 16/chapter-exercises/functor-instance.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 -- 1.
 data Quant a b = Finance
                | Desk a
@@ -8,3 +10,15 @@ instance Functor (Quant a) where
   fmap _ Finance   = Finance
   fmap _ (Desk x)  = Desk x
   fmap f (Bloor y) = Bloor (f y)
+
+-- 2.
+data K a b = K a deriving (Eq, Show)
+
+instance Functor (K a) where
+  fmap _ (K a) = K a
+
+-- 3.
+newtype Flip f a b = Flip (f b a) deriving (Eq, Show)
+
+newtype Functor (Flip K a) where
+  fmap f (Flip (K x)) = Flip $ K $ f x
