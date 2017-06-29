3bda41d0d7d5c94d817b92b26f75e56a6c0e413c
Evan Turner <evturn@gmail.com>
2017-06-28 22:23:22 -0500

    Add notes on list and tuple applicatives

--- 17/notes.hs
+++ 17/notes.hs
@@ -13,22 +13,71 @@ class Functor f => Applicative f where
 -- Every type that can have an Applicative instance must also have a Functor
 --  instance.
 
--------------------------------
+
+-- ∨ -------------------------------- ∨ ---------------------------------- ∨ --
+
 -- fmap
 (<$>) :: Functor f =>       (a -> b) -> f a -> f b
+
 -- 'apply'
 (<*>) :: Applicative f => f (a -> b) -> f a -> f b
 
+-- The difference is the 𝑓 representing functorial structure
+-- that is on the outside of the 'apply' function defintion.
 
--- The difference is the 𝑓 representing functorial structure that is on the
--- outside of the 'apply' function defintion.
--------------------------------
+-- ∧ -------------------------------- ∧ ---------------------------------- ∧ --
 
--- List Applicative
+-- List Applicative typeclass methods
 (<*>) ::  f  (a -> b)  -> f   a ->  f  b
 (<*>) :: [ ] (a -> b)  -> [ ] a -> [ ] b
+
 -- more syntactically typical
 (<*>) ::    [(a -> b)] -> [a]   ->    [b]
 
 pure :: a ->   f  a
 pure :: a ->  [ ] a
+
+-- ∨ -------------------------------- ∨ ---------------------------------- ∨ --
+
+-- List Applicative
+
+[(+1), (*2)] <*> [2, 4]
+
+-- It maps each function value from the first list over the second list.
+-- It then applies the operations and returns one list.
+
+-- [3,5,4,8]
+
+-- ∧ -------------------------------- ∧ ---------------------------------- ∧ --
+
+
+(<*>) :: Applicative f => f (a -> b) -> f a -> f b
+
+listApply :: [(a -> b)] -> [a] -> [b]
+listFmap  ::  (a -> b)  -> [a] -> [b]
+
+-- The 𝑓 structure wrapping the function in `listApply` is itself a list, meaning
+-- `a -> b` from Functor has become a list of `a -> b`
+
+
+-- ∨ -------------------------------- ∨ ---------------------------------- ∨ --
+
+-- Tuple Applicative
+
+(,) <$> [1, 2] <*> [3, 4]
+
+-- Using the `fmap` infix operator `<$>` to map the tuple constructor `(,)` over
+-- the first list `[1, 2]`.
+-- This embeds an unapplied function (the tuple constructor) into some structure
+-- (the list) and returns a list of partially applied functions.
+
+-- First `fmap` lifts the `(,)` over the first list.
+-- Then returns a list of partially applied functions.
+-- [(1, ), (2, )] <*> [3, 4]
+
+-- Then apply the first list `[(1, ), (2, )]`
+-- <*>
+-- to the second list `[3, 4]`
+-- [(1,3),(1,4),(2,3),(2,4)]
+
+-- ∧ -------------------------------- ∧ ---------------------------------- ∧ --
