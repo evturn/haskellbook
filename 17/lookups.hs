5c9be42d882b1c10af9927b815e66f12b855982e
Evan Turner <evturn@gmail.com>
2017-07-01 22:23:22 -0500

    Complete second Lookups exercise

--- 17/lookups.hs
+++ 17/lookups.hs
@@ -1,7 +1,15 @@
 -- Exercises: Lookups
 
 -- 1.
--- added = (+3) (lookup 3 $ zip [1, 2, 3] [4, 5, 6])
-
 added :: Maybe Integer
 added = (+3) <$> (lookup 3 $ zip [1, 2, 3] [4, 5, 6])
+
+-- 2.
+y :: Maybe Integer
+y = lookup 3 $ zip [1, 2, 3] [4, 5, 6]
+
+z :: Maybe Integer
+z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
+
+tupled :: Maybe (Integer, Integer)
+tupled = (,) <$> y <*> z
