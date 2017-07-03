dec339e7520a43f10b492dd38e285fc4d05dd13a
Evan Turner <evturn@gmail.com>
2017-07-02 22:23:22 -0500

    Complete third Lookups exercise

--- 17/lookups.hs
+++ 17/lookups.hs
@@ -1,4 +1,5 @@
 -- Exercises: Lookups
+import Data.List (elemIndex)
 
 -- 1.
 added :: Maybe Integer
@@ -13,3 +14,16 @@ z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
 
 tupled :: Maybe (Integer, Integer)
 tupled = (,) <$> y <*> z
+
+-- 3.
+x3 :: Maybe Int
+x3 = elemIndex 3 [1, 2, 3, 4, 5]
+
+y3 :: Maybe Int
+y3 = elemIndex 4 [1, 2, 3, 4, 5]
+
+max' :: Int -> Int -> Int
+max' = max
+
+maxed :: Maybe Int
+maxed = max' <$> x3 <*> y3
