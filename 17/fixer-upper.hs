5de96307b3bbd91af88e2772a665ab4294083254
Evan Turner <evturn@gmail.com>
2017-07-09 22:23:22 -0500

    Complete Applicative in use exercise

--- 17/fixer-upper.hs
+++ 17/fixer-upper.hs
@@ -1,3 +1,6 @@
 -- 1.
 -- const <$> Just "Hello" <*> "World"
 const <$> Just "Hello" <*> pure "World"
+
+--2. (,,,) Just 90 <*> Just 10 Just "Tierness" [1, 2, 3]
+(,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1, 2, 3]
