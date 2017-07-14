1e2e53a1a3d389b23053faccef73370565bc109d
Evan Turner <evturn@gmail.com>
2017-07-13 22:23:22 -0500

    Continue ZipList Applicative exercise

--- 17/list-applicative.hs
+++ 17/list-applicative.hs
@@ -33,3 +33,26 @@ instance Applicative List where
   _ <*> Nil        = Nil
   Nil <*> _        = Nil
   Cons f xs <*> ys = append (fmap f ys) (fs <*> ys)
+
+
+take' :: Int -> List a -> List a
+take' 0 _     Nil
+take' _ Nil = Nil
+take' n (Cons x xs) = Cons x (take' (n - 1) xs)
+
+newtype ZipList' a = ZipList' (List a)
+                   deriving (Eq, Show)
+
+instance Eq a => EqProp (ZipList' a) where
+  xs =-= ys = xs' `eq` ys'
+    where xs' = let (ZipList' l) = xs
+                in take' 3000 l
+          ys' = let (ZipList' l) = ys
+                in take' 3000 l
+
+instance Functor ZipList' where
+  fmap f (ZipList' xs) = ZipList' $ fmap f xs
+
+instance Applicative ZipList' where
+  pure x = ZipList' (Cons x Nil)
+  (<*>)  = undefined
