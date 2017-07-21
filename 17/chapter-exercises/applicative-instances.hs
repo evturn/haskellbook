a92bb39d24c6c715d1d49a61b6d0ae1f9e310db2
Evan Turner <evturn@gmail.com>
2017-07-20 22:23:22 -0500

    Run Checkers library on Pair Applicative

--- 17/chapter-exercises/applicative-instances.hs
+++ 17/chapter-exercises/applicative-instances.hs
@@ -1,12 +1,11 @@
 import Control.Applicative
 import Data.Monoid
 import Test.QuickCheck
-import Test.QuickCheck.Checkers
+import Test.QuickCheck.Checkers (quickBatch, eq, (=-=), EqProp)
 import Test.QuickCheck.Classes
 
 -- 5.
-
-data Pair a = Pair a a deriving Show
+data Pair a = Pair a a deriving (Eq, Show)
 
 instance Functor Pair where
   fmap f (Pair x y) = Pair (f x) (f y)
@@ -15,3 +14,13 @@ instance Applicative Pair where
   pure x = Pair x x
   Pair f g <*> Pair x y = Pair (f x) (g y)
 
+instance Arbitrary a => Arbitrary (Pair a) where
+  arbitrary = do
+    a <- arbitrary
+    return $ Pair a a
+
+instance Eq a => EqProp (Pair a) where
+  (=-=) = eq
+
+main = do
+  quickBatch $ applicative $ (Pair (1, 2, "uh") (3, 4, "oh") :: Pair (Int, Int, String))
