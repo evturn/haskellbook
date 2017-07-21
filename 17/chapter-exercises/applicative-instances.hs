dd88700997e61a5d9cd149a2d14cbfa5d7c80187
Evan Turner <evturn@gmail.com>
2017-07-21 06:54:35 -0400

    Create Applicative instance for Three

--- 17/chapter-exercises/applicative-instances.hs
+++ 17/chapter-exercises/applicative-instances.hs
@@ -26,7 +26,7 @@ instance Eq a => EqProp (Pair a) where
 data Two a b = Two a b deriving (Eq, Show)
 
 instance Functor (Two a) where
-  fmap f (Two x y )= Two x (f y)
+  fmap f (Two x y) = Two x (f y)
 
 instance Monoid a => Applicative (Two a) where
   pure = Two mempty
@@ -41,6 +41,27 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
 instance (Eq a, Eq b) => EqProp (Two a b) where
   (=-=) = eq
 
+-- 7.
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three x y z) = Three x y (f z)
+
+instance (Monoid a, Monoid b) => Applicative (Three a b) where
+  pure = Three mempty mempty
+  Three a b f <*> Three x y z = Three (a <> x) (b <> y) (f z)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    return (Three x y z)
+
+instance (Eq a, Eq b, Eq c) => EqProp (Three a b c) where
+  (=-=) = eq
+
 main = do
   quickBatch $ applicative $ (Pair (1, 2, "uh") (3, 4, "oh") :: Pair (Int, Int, String))
   quickBatch $ applicative (Two ("sure", "dude") ("cool", "man", "why do I need a third one here?"))
+  quickBatch $ applicative (Three ("one", "more", "guess") ("on", "how", "to") ("make", "this", "work"))
