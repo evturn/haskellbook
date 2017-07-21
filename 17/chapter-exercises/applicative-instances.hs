7fa69193bf59cfbca323d4f023fc9eb2dec085d8
Evan Turner <evturn@gmail.com>
2017-07-21 07:45:09 -0400

    Add Applicative instance with type that takes four arguments

--- 17/chapter-exercises/applicative-instances.hs
+++ 17/chapter-exercises/applicative-instances.hs
@@ -80,8 +80,30 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Three' a b) where
 instance (Eq a, Eq b) => EqProp (Three' a b) where
   (=-=) = eq
 
+-- 9.
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+instance (Monoid a, Monoid b, Monoid c) => Applicative (Four a b c) where
+  pure = Four mempty mempty mempty
+  Four a b c f <*> Four w x y z = Four (a <> w) (b <> x) (c <> y) (f z)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = do
+    w <- arbitrary
+    x <- arbitrary
+    y <- arbitrary
+    z <- arbitrary
+    return (Four w x y z)
+
+instance (Eq a, Eq b, Eq c, Eq d) => EqProp (Four a b c d) where
+  (=-=) = eq
+
 main = do
   quickBatch $ applicative $ (Pair (1, 2, "uh") (3, 4, "oh") :: Pair (Int, Int, String))
   quickBatch $ applicative (Two ("sure", "dude") ("cool", "man", "why do I need a third one here?"))
   quickBatch $ applicative (Three ("one", "more", "guess") ("on", "how", "to") ("make", "this", "work"))
   quickBatch $ applicative (Three' ("please", "work", "and") ("don't", "break", "while") ("this", "tries", "compiling"))
+  quickBatch $ applicative (Four ("here", "is", "another") ( "thing", "that", "I") ("hope", "runs", "smoothly") ("when", "I", "compile"))
