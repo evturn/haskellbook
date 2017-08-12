17c87a7ef5402cb00250e39c858efe9510123699
Evan Turner <evturn@gmail.com>
2017-08-11 22:23:22 -0500

    Update algebraic datatypes exercises

--- 11/vehicles.hs
+++ 11/vehicles.hs
@@ -1,33 +1,38 @@
-module Vehicle where
+module Vehicles where
 
-data Price = Price Integer
-             deriving (Eq, Show)
+data Price =
+  Price Integer
+  deriving (Eq, Show)
 
-data Manufacturer = Mini
-                  | Mazda
-                  | Tata
-                    deriving (Eq, Show)
+data Manufacturer =
+    Mini
+  | Mazda
+  | Tata
+  deriving (Eq, Show)
 
-data Airline = PapuAir
-             | CatapultsR'Us
-             | TakeYourChancesUnited
-               deriving (Eq, Show)
+data Airline =
+    PapuAir
+  | CatapultsR'Us
+  | TakeYourChancesUnited
+  deriving (Eq, Show)
 
-data Size = FuckingHuge
-          | Regular
-          | TinyAsFuck
-            deriving (Eq, Show)
+data Size =
+    Massive
+  | Regular
+  | Microscopic
+  deriving (Eq, Show)
 
-data Vehicle = Car Manufacturer Price
-             | Plane Airline Size
-               deriving (Eq, Show)
+data Vehicle =
+    Car Manufacturer Price
+  | Plane Airline Size
+  deriving (Eq, Show)
 
 myCar :: Vehicle
-myCar    = Car Mini (Price 14000)
-urCar    = Car Mazda (Price 200000)
+myCar = Car Mini (Price 14000)
+urCar = Car Mazda (Price 200000)
 clownCar = Car Tata (Price 7000)
-doge     = Plane PapuAir FuckingHuge
-catt     = Plane PapuAir TinyAsFuck
+doge = Plane PapuAir Massive
+catt = Plane PapuAir Microscopic
 
 isCar :: Vehicle -> Bool
 isCar (Car _ _) = True
