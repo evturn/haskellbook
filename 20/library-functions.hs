b21d576bee212441f3af23d747fa6f3ac58b124a
Evan Turner <evturn@gmail.com>
2017-08-14 02:38:22 -0400

    Add unexported Foldable constructors

--- 20/library-functions.hs
+++ 20/library-functions.hs
@@ -1,11 +1,35 @@
+import Data.Bool
+import Data.Foldable
+import Data.Monoid
+
 -- 1.
-sum' :: (Folable t, Num a) => t a -> a
-sum' = foldMap Sum
+sum' :: (Foldable t, Num a) => t a -> a
+sum' = getSum . foldMap Sum
 
 -- 2.
 product' :: (Foldable t, Num a) => t a -> a
-product' = foldMap Product
+product' = getProduct . foldMap Product
 
 -- 3.
-elem' :: (Foldable t, Eq a) => t a -> Bool
-elem' n xs = foldMap Any $ fmap (== n) xs
+elem' :: (Foldable t, Eq a) => a -> t a -> Bool
+elem' n xs = getAny $ foldMap (Any . (n ==)) xs
+
+-- 4.
+-- https://hackage.haskell.org/package/base-4.10.0.0/docs/src/Data.Functor.Utils.html
+newtype Max a = Max {getMax :: Maybe a} deriving (Eq, Show)
+newtype Min a = Min {getMin :: Maybe a} deriving (Eq, Show)
+
+instance Ord a => Monoid (Max a) where
+  mempty              = Max Nothing
+  m `mappend` Max Nothing    = m
+  Max Nothing `mappend` n    = n
+  (Max m) `mappend` (Max n) = Max (max m n)
+
+instance Ord a => Monoid (Min a) where
+  mempty              = Min Nothing
+  m `mappend` Min Nothing    = m
+  Min Nothing `mappend` n    = n
+  (Min m) `mappend` (Min n) = Min (min m n)
+
+minimum' :: (Foldable t, Ord a) => t a -> Maybe a
+minimum' xs = getMin $ foldMap (Min . Just) xs
