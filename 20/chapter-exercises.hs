8b0bb2f1fdb40206eb23166147a02b05767325f3
Evan Turner <evturn@gmail.com>
2017-08-19 04:56:35 -0400

    Complete second Foldable chapter exercise

--- 20/chapter-exercises.hs
+++ 20/chapter-exercises.hs
@@ -8,3 +8,10 @@ data Constant a b =
 
 instance Foldable (Constant a) where
   foldMap f (Constant x) = mempty
+
+-- 2.
+data Two a b =
+  Two a b
+
+instance Foldable (Two a) where
+  foldMap f (Two x y) = f y
