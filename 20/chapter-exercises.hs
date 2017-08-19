15b3f25f655c435a3f84bd57826de5b956bb5063
Evan Turner <evturn@gmail.com>
2017-08-19 05:11:23 -0400

    Complete Foldable chapter exercises

--- 20/chapter-exercises.hs
+++ 20/chapter-exercises.hs
@@ -15,3 +15,24 @@ data Two a b =
 
 instance Foldable (Two a) where
   foldMap f (Two x y) = f y
+
+-- 3.
+data Three a b c =
+  Three a b c
+
+instance Foldable (Three a b) where
+  foldMap f (Three x y z) = f z
+
+-- 4.
+data Three' a b =
+  Three' a b b
+
+instance Foldable (Three' a) where
+  foldMap f (Three' x y y') = (f y) <> (f y')
+
+-- 5.
+data Four' a b =
+  Four' a b b b
+
+instance Foldable (Four' a) where
+  foldMap f (Four' x y y' y'') = (f y) <> (f y') <> (f y'')
