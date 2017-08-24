d581be6350377b0417cfd8295fbea09e0b342ce3
Evan Turner <evturn@gmail.com>
2017-08-23 22:23:22 -0500

    Create http stack project

---
+++ 21/http-stuff/app/Main.hs
@@ -0,0 +1,5 @@
+module Main where
+
+import HttpStuff 
+
+main = traversedUrls
---
+++ 21/http-stuff/src/HttpStuff.hs
@@ -0,0 +1,15 @@
+module HttpStuff where
+
+import Data.ByteString.Lazy hiding (map)
+import Network.Wreq
+
+urls :: [String]
+urls = [ "http://httpbin.org/ip"
+       , "http://httpbin.org/bytes/5"
+       ]
+
+mappingGet :: [IO (Response ByteString)]
+mappingGet = map get urls
+
+traversedUrls :: IO [Response ByteString]
+traversedUrls = traverse get urls
