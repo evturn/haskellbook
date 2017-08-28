5f687fda032b15e84d8dcc9f2572bd8263f027cc
Evan Turner <evturn@gmail.com>
2017-08-27 22:23:22 -0500

    Create Traversable instance for Identity

--- 21/chapter-exercises/traversable-instances.hs
+++ 21/chapter-exercises/traversable-instances.hs
@@ -1,12 +1,35 @@
 import Data.Traversable
-import Test.QuickCheck
+import Test.QuickCheck (Arbitrary, arbitrary)
 import Test.QuickCheck.Classes
 import Test.QuickCheck.Checkers
 
-type TI = []
 
+type TupleOfStuff = (Int, Int, [Int])
+
+
+newtype Identity a = Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+
+instance Foldable Identity where
+  foldr f x (Identity xs) = f xs x
+
+instance Traversable Identity where
+  traverse f (Identity x) = Identity <$> f x 
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    x <- arbitrary
+    return $ Identity x
+
+instance Eq a => EqProp (Identity a) where
+  (=-=) = eq
+
+
+idTrigger = undefined :: Identity TupleOfStuff 
 
 main = do
-  let trigger :: TI (Int, Int, [Int])
-      trigger = undefined
-  quickBatch (traversable trigger)
+  putStrLn "\nIdentity"
+  quickBatch (traversable idTrigger)
