c85d6fb9c52ccf8684189cfb5c8fa4588d2736a6
Evan Turner <evturn@gmail.com>
2017-08-29 22:23:23 -0500

    Create Traversable instance for Constant type

--- 21/chapter-exercises/traversable-instances.hs
+++ 21/chapter-exercises/traversable-instances.hs
@@ -3,10 +3,7 @@ import Test.QuickCheck (Arbitrary, arbitrary)
 import Test.QuickCheck.Classes
 import Test.QuickCheck.Checkers
 
-
-type TupleOfStuff = (Int, Int, [Int])
-
-
+-- Identity
 newtype Identity a = Identity a
   deriving (Eq, Ord, Show)
 
@@ -27,9 +24,36 @@ instance Arbitrary a => Arbitrary (Identity a) where
 instance Eq a => EqProp (Identity a) where
   (=-=) = eq
 
+-- Constant
+newtype Constant a b =
+  Constant { getConstant :: a } deriving (Eq, Show, Ord)
+
+instance Functor (Constant a) where
+  fmap _ (Constant x) = Constant x
+
+instance Foldable (Constant a) where
+  foldr _ x (Constant _) = x
+
+instance Traversable (Constant a) where
+  traverse f (Constant x) = pure $ Constant x 
+
+instance Arbitrary a => Arbitrary (Constant a b) where
+  arbitrary = do
+    x <- arbitrary
+    return $ Constant x
+
+instance (Eq a, Eq b) => EqProp (Constant a b) where
+  (=-=) = eq
+
+type TupleOfStuff = (Int, Int, [Int])
+type OneThing = Int
 
 idTrigger = undefined :: Identity TupleOfStuff 
+constantTrigger = undefined :: Constant Int TupleOfStuff
 
 main = do
-  putStrLn "\nIdentity"
+  putStrLn "\n=============== Identity ===================="
   quickBatch (traversable idTrigger)
+
+  putStrLn "\n=============== Constant ===================="
+  quickBatch (traversable constantTrigger)
