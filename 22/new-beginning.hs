604694a51b74453be8c625c5c1c8ded1096d8ac3
Evan Turner <evturn@gmail.com>
2017-09-03 13:09:31 -0400

    Start chapter 22

---
+++ 22/new-beginning.hs
@@ -0,0 +1,16 @@
+import Control.Applicative
+
+boop = (*2)
+doop = (+10)
+
+bip :: Integer -> Integer
+bip = boop . doop
+
+bloop :: Integer -> Integer
+bloop = fmap boop doop
+
+bbop :: Integer -> Integer
+bbop = (+) <$> boop <*> doop
+
+duwop :: Integer -> Integer
+duwop = liftA2 (+) boop doop
