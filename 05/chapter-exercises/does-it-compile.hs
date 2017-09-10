b52adb35f945e89fc900183abf4fed13e7340117
Evan Turner <evturn@gmail.com>
2017-09-10 18:05:41 -0400

    Finish compile chapter exercise

--- 05/chapter-exercises/does-it-compile.hs
+++ 05/chapter-exercises/does-it-compile.hs
@@ -1,4 +1,18 @@
 -- 1.
-
 bigNum = (^) 5
 wahoo = bigNum $ 10
+
+-- 2.
+x = print
+y = print "woohoo!"
+z = x "hello world!"
+
+-- 3.
+a = (+)
+b = 5
+-- c = b 10
+-- d = c 200
+
+-- 4.
+a = 12 + b
+-- b = 10000 * c
