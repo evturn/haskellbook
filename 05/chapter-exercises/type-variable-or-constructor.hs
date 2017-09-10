2ddb80ec7229d436b43b2e55e0ecb5642a069a0a
Evan Turner <evturn@gmail.com>
2017-09-10 18:13:46 -0400

    Redo types chapter exercises on type variables and constructors

--- 05/chapter-exercises/type-variable-or-constructor.hs
+++ 05/chapter-exercises/type-variable-or-constructor.hs
@@ -3,33 +3,33 @@
   -- constrained polymorphic type variable
   -- concrete type constructor
 
--- f4 :: Num a => a -> b -> Int -> Int
-    --          [0]  [1]   [2]    [3]
-    -- f4
-    -- 0 constrained polymorphic type variable
-    -- 1 fully polymorphic type variable
-    -- 2 concrete type constructor
-    -- 3 concrete type constructor
+-- 1. 
+f0 :: Num a => a -> b -> Int -> Int
+--            [0]  [1]   [2]    [3]
+-- 0 constrained polymorphic type variable
+-- 1 fully polymorphic type variable
+-- 2 concrete type constructor
+-- 3 concrete type constructor
 
--- f5 :: zed -> Zed -> Blah
-    --  [0]    [1]    [2]
-    -- f5
-    -- 0 fully polymorphic type variable
-    -- 1 concrete type constructor
-    -- 2 concrete type constructor
+-- 2.
+f1 :: zed -> Zed -> Blah
+ --   [0]    [1]    [2]
+-- 0 fully polymorphic type variable
+-- 1 concrete type constructor
+-- 2 concrete type constructor
 
--- f6 :: Enum b => a -> b -> C
-    --           [0]  [1]  [2]
-    -- f6
-    -- 0 fully polymorphic type variable
-    -- 1 constrained polymorphic type variable
-    -- 2 concrete type constructor
+-- 3.
+f2 :: Enum b => a -> b -> C
+--        [0]  [1]  [2]  [3]
+-- 0 constrained polymorphic type variable
+-- 1 fully polymorphic type variable
+-- 2 constrained polymorphic type variable
+-- 3 concrete type constructor
 
--- f7 :: f -> g -> C
-    -- [0]  [1]  [2]
-    -- f7
-    -- 0 fully polymorphic type variable
-    -- 1 fully polymorphic type variable
-    -- 2 concrete type constructor
-
+-- 4.
+f3 :: f -> g -> C
+--   [0]  [1]  [2]
+-- 0 fully polymorphic type variable
+-- 1 fully polymorphic type variable
+-- 2 concrete type constructor
 
