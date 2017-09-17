09068b0490adf4d4c22f3b244aa65ca8cc6e0410
Evan Turner <evturn@gmail.com>
2017-09-16 22:23:22 -0400

    Redo exercise on Eq instances

--- 06/eq-instances.hs
+++ 06/eq-instances.hs
@@ -1,63 +1,57 @@
+-- 1.
 data TisAnInteger =
   TisAn Integer
 
 instance Eq TisAnInteger where
-  (==) (TisAn a) (TisAn a') = a == a'
+  (==) (TisAn x) (TisAn y) = x == y 
 
-
-
+-- 2.
 data TwoIntegers =
   Two Integer Integer
 
 instance Eq TwoIntegers where
-  (==) (Two a b) (Two a' b') =
-    a == a' && b == b'
+  (==) (Two x y) (Two x' y') = x == x && y == y'
 
-
-
+-- 3.
 data StringOrInt =
-    TisAnInt   Integer
+    TisAnInt Int
   | TisAString String
 
 instance Eq StringOrInt where
-  (==) (TisAnInt a) (TisAnInt a') = a == a'
-  (==) (TisAString a) (TisAString a') = a == a'
-  (==) _ _ = False
+  (==) (TisAnInt x)   (TisAnInt y)   = x == y
+  (==) (TisAString x) (TisAString y) = x == y
 
 
-
+-- 4.
 data Pair a =
   Pair a a
 
 instance Eq a => Eq (Pair a) where
-  (==) (Pair a a') (Pair b b') = a == b && a' == b'
+  (==) (Pair x y) (Pair x' y') = x == x' && y == y'
 
-
-
+-- 5.
 data Tuple a b =
   Tuple a b
 
-instance (Eq a, Eq b) => Eq Tuple (a, b) where
-  (==) (Tuple a, a') (Tuple b b') = a == b && a' == b'
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+  (==) (Tuple x y) (Tuple x' y') = x == x' && y == y'
 
-
-
-data Which a =
+-- 6.
+data Which a = 
     ThisOne a
   | ThatOne a
 
 instance Eq a => Eq (Which a) where
-  (==) (ThisOne a) (ThisOne a') = a == a'
-  (==) (ThatOne a) (ThatOne a') = a == a'
-  (==) _ _ = False
+  (==) (ThisOne x) (ThisOne y) = x == y
+  (==) (ThatOne x) (ThatOne y) = x == y
+  (==) _ _                     = False
 
-
-
+-- 7.
 data EitherOr a b =
     Hello a
   | Goodbye b
 
 instance (Eq a, Eq b) => Eq (EitherOr a b) where
-  (==) (Hello a) (Hello a') = a == a'
-  (==) (Goodbye a) (Goodbye a') = a == a'
-  (==) _ _ = False
+  (==) (Hello x) (Hello y)     = x == y
+  (==) (Goodbye x) (Goodbye y) = x == y
+  (==) _ _                     = False
