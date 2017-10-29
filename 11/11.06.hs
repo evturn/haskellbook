a7a85ce1c285de716e01843e2323d41291d8f4ab
Evan Turner <evturn@gmail.com>
2017-10-29 01:38:23 -0400

    Complete redo of exercise 11.6

--- 11/11.06.hs
+++ 11/11.06.hs
@@ -14,13 +14,21 @@ data Airline =
 
 data Vehicle =
     Car Manufacturer Price
-  | Plane Airline
+  | Plane Airline Size
+  deriving (Eq, Show)
+
+-- Added for question 5.
+data Size =
+    S
+  | M
+  | L
+  | XL
   deriving (Eq, Show)
 
 myCar    = Car Mini (Price 14000)
 urCar    = Car Mazda (Price 20000)
 clownCar = Car Tata (Price 7000)
-doge     = Plane PapuAir
+doge     = Plane PapuAir XL
 
 -- 1.
 -- What is the type of myCar?
@@ -29,12 +37,25 @@ doge     = Plane PapuAir
 -- 2.
 -- Define the functions.
 isCar :: Vehicle -> Bool
-isCar (Plane _) = False
 isCar (Car _ _) = True
+isCar _         = False
 
 isPlane :: Vehicle -> Bool
-isPlane (Car _ _) = False
-isPlane (Plane _) = True
+isPlane (Plane _ _) = True
+isPlane _           = False
 
 areCars :: [Vehicle] -> [Bool]
 areCars = fmap isCar
+
+-- 3.
+-- Write a function that returns the manufacturer of a piece of data.
+getManu :: Vehicle -> Manufacturer
+getManu (Car x _) = x
+getManu _         = error "\n\nNo manufacturer found.\n"
+
+-- 4.
+-- What will happen if you use `getManu` on `Plane` data?
+-- Non-exhaustive patterns exception
+
+-- 5.
+-- Add the size of a plane as an argument to the `Plane` constructor.
