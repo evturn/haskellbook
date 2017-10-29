23a6a4e5b9e03050372a2e5d630bc531227b919c
Evan Turner <evturn@gmail.com>
2017-10-29 06:56:14 -0400

    Add solution to exercise 2 for 11.9

--- 11/11.09-logic-goats.hs
+++ 11/11.09-logic-goats.hs
@@ -19,3 +19,11 @@ data IntString = IntString (Int, String) deriving (Eq, Show)
 
 instance TooMany IntString where
   tooMany (IntString (n, _)) = tooMany n
+
+-- 2.
+-- Write another `TooMany` instance for (Int, Int) and summ the values
+-- together under the assumption this is a count of goats from two fields.
+data IntInt = IntInt (Int, Int) deriving (Eq, Show)
+
+instance TooMany IntInt where
+  tooMany (IntInt (n, n')) = tooMany (n + n')
