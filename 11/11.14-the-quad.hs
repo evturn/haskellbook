f00697eb6b2f4f8ad755977de03b247b443c80da
Evan Turner <evturn@gmail.com>
2017-10-30 10:12:00 -0400

    Start redo of exercise 11.14

--- 11/11.14-the-quad.hs
+++ 11/11.14-the-quad.hs
@@ -1,187 +1,43 @@
-data Quantum =
-    Yes
-  | No
-  | Both
-  deriving (Eq, Show)
+-- Determine how many unique inhabitants each type has?
+data Quad = One
+          | Two
+          | Three
+          | Four
+          deriving (Eq, Show)
 
-quantSum1 :: Either Quantum Quantum
-quantSum1 = Right Yes
-
-quantSum2 :: Either Quantum Quantum
-quantSum2 = Right No
-
-quantSum3 :: Either Quantum Quantum
-quantSum3 = Right Both
-
-quantSum4 :: Either Quantum Quantum
-quantSum4 = Left Yes
-
-quantSum5 :: Either Quantum Quantum
-quantSum5 = Left No
-
-quantSum6 :: Either Quantum Quantum
-quantSum6 = Left Both
-
-
-quantProd1 :: (Quantum, Quantum)
-quantProd1 = (Yes, Yes)
-
-quantProd2 :: (Quantum, Quantum)
-quantProd2 = (Yes, No)
-
-quantProd3 :: (Quantum, Quantum)
-quantProd3 = (Yes, Both)
-
-quantProd4 :: (Quantum, Quantum)
-quantProd4 = (No, Yes)
-
-quantProd5 :: (Quantum, Quantum)
-quantProd5 = (No, No)
-
-quantProd6 :: (Quantum, Quantum)
-quantProd6 = (No, Both)
-
-quantProd7 :: (Quantum, Quantum)
-quantProd7 = (Both, Yes)
-
-quantProd8 :: (Quantum, Quantum)
-quantProd8 = (Both, No)
-
-quantProd9 :: (Quantum, Quantum)
-quantProd9 = (Both, Both)
-
-quantFlip1 :: Quantum -> Quantum
-quantFlip1 Yes  = Yes
-quantFlip1 No   = Yes
-quantFlip1 Both = Yes
-
-quantFlip2 :: Quantum -> Quantum
-quantFlip2 Yes  = Yes
-quantFlip2 No   = Yes
-quantFlip2 Both = No
-
-quantFlip3 :: Quantum -> Quantum
-quantFlip3 Yes  = Yes
-quantFlip3 No   = Yes
-quantFlip3 Both = Both
-
-quantFlip4 :: Quantum -> Quantum
-quantFlip4 Yes  = Yes
-quantFlip4 No   = No
-quantFlip4 Both = Yes
-
-quantFlip5 :: Quantum -> Quantum
-quantFlip5 Yes  = Yes
-quantFlip5 No   = Both
-quantFlip5 Both = Yes
-
-quantFlip6 :: Quantum -> Quantum
-quantFlip6 Yes  = No
-quantFlip6 No   = Yes
-quantFlip6 Both = Yes
-
-quantFlip7 :: Quantum -> Quantum
-quantFlip7 Yes  = Both
-quantFlip7 No   = Yes
-quantFlip7 Both = Yes
-
-quantFlip8 :: Quantum -> Quantum
-quantFlip8 Yes  = Both
-quantFlip8 No   = Yes
-quantFlip8 Both = No
-
-quantFlip9 :: Quantum -> Quantum
-quantFlip9 Yes  = Both
-quantFlip9 No   = No
-quantFlip9 Both = No
-
-quantFlip10 :: Quantum -> Quantum
-quantFlip10 Yes  = Both
-quantFlip10 No   = No
-quantFlip10 Both = Both
-
-quantFlip11 :: Quantum -> Quantum
-quantFlip11 Yes  = Both
-quantFlip11 No   = Both
-quantFlip11 Both = Both
-
-quantFlip12 :: Quantum -> Quantum
-quantFlip12 Yes  = No
-quantFlip12 No   = No
-quantFlip12 Both = No
-
-convert :: Quantum -> Bool
-convert = undefined
-
--- 1
-convert Yes  = False
-convert No   = False
-convert Both = False
-
--- 2
-convert Yes  = False
-convert No   = False
-convert Both = True
-
--- 3
-convert Yes  = False
-convert No   = True
-convert Both = False
-
--- 4
-convert Yes  = True
-convert No   = False
-convert Both = False
-
--- 5
-convert Yes  = False
-convert No   = True
-convert Both = True
-
--- 6
-convert Yes  = True
-convert No   = False
-convert Both = True
-
--- 7
-convert Yes  = True
-convert No   = True
-convert Both = False
-
--- 8
-convert Yes  = True
-convert No   = True
-convert Both = True
-
-data Quad =
-    One
-  | Two
-  | Three
-  | Four
-  deriving (Eq, Show)
-
+-- 1.
 eQuad :: Either Quad Quad
--- 4 + 4
--- 8 different forms
+eQuad = undefined
+-- Answer: 
+-- Either = Quad 4 | Quad 4
+-- 8      =      4 + 4
+--
+-- There are 8 different possible implementations.
 
+-- 2.
 prodQuad :: (Quad, Quad)
--- 4 * 4
--- 16 different forms
+prodQuad = undefined
+-- Answer:
+-- (Quad 4, Quad 4)
+-- 16 =  4 * 4
+--
+-- There are 16 different possible implementations.
 
+-- 3.
 funcQuad :: Quad -> Quad
--- 4 ^ 4
--- 256 different forms
+funcQuad = undefined
+-- Answer:
+-- Quad 4 -> Quad 4
+-- 256 =  4 ^ 4
+--
+-- There are 256 different possible implementations.
 
-funcTBool :: (Bool, Bool, Bool)
--- 2 * 2 * 2
--- 8 different forms
+-- 4.
+prodTBool :: (Bool, Bool, Bool)
+prodTBool = undefined
+-- Answer:
+-- (Bool 2, Bool 2, Bool 2)
+-- 8 =   2 *     2 *     2
+--
+-- There are 8 different possible implementations.
 
-gTwo :: Bool -> Bool -> Bool
--- (2 ^ 2) ^ 2
--- 2 ^ (2 * 2)
--- 8 different forms
-
-fTwo :: Bool -> Quad -> Quad
--- (4 ^ 4) ^ 2
--- 4 ^ (4 * 2)
--- 65536 different forms
