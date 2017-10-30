9651f23a263add5877ab8d8ebca16102320396cc
Evan Turner <evturn@gmail.com>
2017-10-30 06:34:19 -0400

    Add remaining 11.13 examples

--- 11/11.13-programmers.hs
+++ 11/11.13-programmers.hs
@@ -43,3 +43,52 @@ allLanguages =
 allProgrammers :: [Programmer]
 allProgrammers = [Programmer { lang = x, os = y } | x <- allLanguages,
                                                     y <- allOperatingSystems]
+
+-----------------------------------------------------------------------------
+--
+-- Deconstructing values
+--
+-----------------------------------------------------------------------------
+newtype Name = Name String
+  deriving Show
+
+newtype Acres = Acres Int 
+  deriving Show
+
+data FarmerType = DairyFarmer
+                | WheatFarmer
+                | SoybeanFarmer
+                deriving Show
+
+data Farmer = Farmer Name Acres FarmerType
+  deriving Show
+
+isDairyFarmer :: Farmer -> Bool
+isDairyFarmer (Farmer _ _ DairyFarmer) = True
+isDairyFarmer _                        = False
+
+data FarmerRec = FarmerRec
+    { name       :: Name
+    , acres      :: Acres
+    , farmerType :: FarmerType 
+    } deriving Show
+
+isDairyFarmerRec :: FarmerRec -> Bool
+isDairyFarmerRec farmer = case farmerType farmer of
+                            DairyFarmer -> True
+                            _           -> False
+
+-----------------------------------------------------------------------------
+--
+-- Accidental bottoms from records
+--
+-----------------------------------------------------------------------------
+data Car = Car 
+    { make  :: String
+    , model :: String
+    , year  :: Integer
+    } deriving (Eq, Show)
+
+data Automobile = Null
+                | Automobile Car
+                deriving (Eq, Show)
