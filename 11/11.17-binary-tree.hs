014686275afba512e64a3554480d537d38c03265
Evan Turner <evturn@gmail.com>
2017-10-30 12:25:52 -0400

    Complete exercise converting binary tree to list

--- 11/11.17-binary-tree.hs
+++ 11/11.17-binary-tree.hs
@@ -26,3 +26,44 @@ mapExpected = Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
 mapOkay = if mapTree (+1) testTree' == mapExpected
           then print "yup okay!"
           else error "test failed!"
+-----------------------------------------------------------------------------
+--
+-- Convert binary trees to lists
+--
+-----------------------------------------------------------------------------
+preorder :: BinaryTree a -> [a]
+preorder Leaf         = []
+preorder (Node l x r) = x : preorder l ++ preorder r
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf         = []
+inorder (Node l x r) = inorder l ++ (x : inorder r)
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf         = []
+postorder (Node l x r) = (postorder l ++ postorder r) ++ [x]
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder = if preorder testTree == [2, 1, 3]
+               then putStrLn "Preorder fine!"
+               else putStrLn "Bad news bears."
+
+testInorder :: IO ()
+testInorder = if inorder testTree == [1, 2, 3]
+              then putStrLn "Inorder fine!"
+              else putStrLn "Bad news bears."
+
+testPostorder :: IO ()
+testPostorder = if postorder testTree == [1, 3, 2]
+                then putStrLn "Postorder fine!"
+                else putStrLn "Bad news bears."
+
+main :: IO ()
+main = do
+    testPreorder
+    testInorder
+    testPostorder
+
