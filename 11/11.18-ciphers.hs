56b8cb872d42c9bcbab2ef0557231063afdf922a
Evan Turner <evturn@gmail.com>
2017-10-31 00:46:08 -0400

    Redo cipher exercise in 11.18

---
+++ 11/11.18-ciphers.hs
@@ -0,0 +1,28 @@
+import Data.Char
+
+charCodes :: [Int]
+charCodes = fmap ord ['a'..'z']
+  
+wrapLeft :: Int -> Int
+wrapLeft n
+    | n > (last charCodes) = n - (last charCodes) + (head charCodes - 1)
+    | otherwise            = n
+
+wrapRight :: Int -> Int
+wrapRight n
+    | n < (head charCodes) = n + (last charCodes) - (head charCodes - 1)
+    | otherwise            = n
+
+shiftChar :: Int -> Char -> Char
+shiftChar n ch = chr $ wrapLeft (n + ord ch)
+
+unshiftChar :: Int -> Char -> Char
+unshiftChar n co = chr $ wrapRight (ord co - n)
+
+caesar :: Int -> String -> String
+caesar _ [] = []
+caesar n xs = fmap (shiftChar n) xs
+
+uncaesar :: Int -> String -> String
+uncaesar _ [] = []
+uncaesar n xs = fmap (unshiftChar n) xs
