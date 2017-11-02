d5035dfad9f30ba91fdbfecd05a5e964af4d7248
Evan Turner <evturn@gmail.com>
2017-11-02 17:52:34 -0400

    Complete last exercise in 11.18

--- 11/11.18-huttons-razor.hs
+++ 11/11.18-huttons-razor.hs
@@ -1,3 +1,5 @@
+-- 1.
+-- Write a function which reduces an expression to a final sum.
 data Expr = Lit Integer
           | Add Expr Expr
           deriving (Eq, Show)
@@ -5,3 +7,9 @@ data Expr = Lit Integer
 eval :: Expr -> Integer
 eval (Lit x)   = x
 eval (Add x y) = (eval x) + (eval y)
+
+-- 2.
+-- Write a printer for the expressions.
+printExpr :: Expr -> String
+printExpr (Lit x)   = show x
+printExpr (Add x y) = (printExpr x) ++ " + " ++ (printExpr y)
