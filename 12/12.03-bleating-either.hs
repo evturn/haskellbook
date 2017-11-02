ff65020acea4af79c9e6b51c11e1f7e85bc02f8c
Evan Turner <evturn@gmail.com>
2017-11-02 19:27:00 -0400

    Update section on Either for chapter 12

--- 12/12.03-bleating-either.hs
+++ 12/12.03-bleating-either.hs
@@ -1,3 +1,7 @@
+type Name             = String
+type Age              = Integer
+type ValidatePerson a = Either [PersonInvalid] a
+
 data Person = Person Name Age
   deriving Show
 
@@ -5,41 +9,27 @@ data PersonInvalid = NameEmpty
                    | AgeTooLow
                    deriving (Eq, Show)
 
-type Name = String
-type Age = Integer
-type ValidatePerson a = Either [PersonInvalid] a
-
-ageOkay :: Age -> Either [PersonInvalid] Age
-ageOkay age = case age >= 0 of
-  True  -> Right age
-  False -> Left [AgeTooLow]
-
-nameOkay :: Name -> Either [PersonInvalid] Name
-nameOkay name = case name /= "" of
-  True  -> Right name
-  False -> Left [NameEmpty]
-
 toString :: PersonInvalid -> String
 toString NameEmpty = "NameEmpty"
 toString AgeTooLow = "AgeTooLow"
 
--- mkPerson :: Name
---          -> Age
---          -> Either PersonInvalid Person
--- mkPerson name age
---   | name /= "" && age >= 0 = Right $ Person name age
---   | name == ""             = Left NameEmpty
---   | otherwise              = Left AgeTooLow
+ageOkay :: Age -> Either [PersonInvalid] Age
+ageOkay age = case age >= 0 of
+                  True  -> Right age
+                  False -> Left [AgeTooLow]
 
-mkPerson :: Name
-         -> Age
-         -> ValidatePerson Person
-mkPerson name age = mkPerson' (nameOkay name) (ageOkay age)
+nameOkay :: Name -> Either [PersonInvalid] Name
+nameOkay name = case name /= "" of
+                    True  -> Right name
+                    False -> Left [NameEmpty]
 
+mkPerson :: Name -> Age -> ValidatePerson Person
+mkPerson name age
+    | name /= "" && age >= 0 = Right $ Person name age
+    | name == ""             = Left NameEmpty
+    | otherwise              = Left AgeTooLow
 
-mkPerson' :: ValidatePerson Name
-          -> ValidatePerson Age
-          -> ValidatePerson Person
+mkPerson' :: ValidatePerson Name -> ValidatePerson Age -> ValidatePerson Person
 mkPerson' (Right nameOk) (Right ageOk) = Right (Person nameOk ageOk)
 mkPerson' (Left badName) (Left badAge) = Left (badName ++ badAge)
 mkPerson' (Left badName) _             = Left badName
