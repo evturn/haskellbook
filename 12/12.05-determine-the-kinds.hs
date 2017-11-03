d6daa011eb80514eaaf771f86d9657cb55db053c
Evan Turner <evturn@gmail.com>
2017-11-02 20:04:15 -0400

    Add first of chapter 12 exercises

---
+++ 12/12.05-determine-the-kinds.hs
@@ -0,0 +1,14 @@
+-- 1.
+-- What is the kind of `a`?
+id :: a -> a
+id = undefined
+-- Answer: 
+-- * :: *
+
+-- 2.
+-- What are the kinds of `a` and `f`?
+r :: a -> f a
+r = undefined
+-- Answer:
+-- a :: *
+-- f :: * -> *
