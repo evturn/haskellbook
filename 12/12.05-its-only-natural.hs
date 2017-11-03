75cbf5cb9129239de4cd38700108410efc37c1af
Evan Turner <evturn@gmail.com>
2017-11-02 22:15:21 -0400

    Begin redo of 12.05 on natural numbers

---
+++ 12/12.05-its-only-natural.hs
@@ -0,0 +1,10 @@
+data Nat = Zero
+         | Succ Nat
+         deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero     = 0
+natToInteger (Succ x) = 1 + natToInteger x
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat = undefined
