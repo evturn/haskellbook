c869c2f7bc122e02c9e396adc7b035e830957db6
Evan Turner <evturn@gmail.com>
2017-11-05 20:56:23 -0500

    Generate test values for polymorphic type arguments

--- 14/14.03-addition/Addition.hs
+++ 14/14.03-addition/Addition.hs
@@ -41,6 +41,28 @@ genThreeple = do
   c <- arbitrary
   return (a, b, c)
 
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [ Left a
+           , Right b
+           ]
+
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [ Nothing
+           , Just a
+           ]
+
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency [ (1, return Nothing)
+            , (3, return $ Just a)
+            ]
+
 main :: IO ()
 main = hspec $ do
   describe "Addition" $ do
