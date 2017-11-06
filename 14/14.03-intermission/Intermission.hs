90b9be86c6bbf7ea1e088280650d2fb18ae83c8f
Evan Turner <evturn@gmail.com>
2017-11-05 19:01:19 -0500

    Add addition and division hspec tests

--- 14/14.03-intermission/Intermission.hs
+++ 14/14.03-intermission/Intermission.hs
@@ -1,6 +1,13 @@
-module Intermission where
+module Addition where
 
 import           Test.Hspec
+
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+  where
+    go n d count
+      | n < d     = (count, n)
+      | otherwise = go (n -d) d (count + 1)
 
 mult :: (Eq a, Ord a, Num a) => a -> a -> a
 mult x y
@@ -10,7 +17,15 @@ mult x y
 
 main :: IO ()
 main = hspec $ do
-  describe "Intermission" $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
+  describe "Division" $ do
+    it "15 divided by 3 is 5" $ do
+      dividedBy 15 3 `shouldBe` (5, 0)
+    it "22 divided by 5 is 4 remainer 2" $ do
+      dividedBy 22 5 `shouldBe` (4, 2)
+  describe "Multiplication" $ do
     it "2 multiplied by 10 using recursive summation is 20" $ do
       mult 2 10 `shouldBe` 20
 
