a6756828981acff33b53e59abf8b3d1adedb4ab4
Evan Turner <evturn@gmail.com>
2017-11-07 00:12:49 -0500

    Add solutions 3 and 4 to QuickCheck chapter exercise

--- 14/14.07-using-quickcheck.hs
+++ 14/14.07-using-quickcheck.hs
@@ -25,12 +25,33 @@ prop_listOrdered :: String -> Bool
 prop_listOrdered = listOrdered . sort
 
 -----------------------------------------------------------------------------
+-- 3.
+plusAssociative :: Int -> Int -> Int -> Bool
+plusAssociative x y z = x + (y + z) == (x + y) + z
+
+plusCommutative :: Int -> Int -> Bool
+plusCommutative x y = x + y == y + x
+
+-----------------------------------------------------------------------------
+-- 4.
+multAssociative :: Int -> Int -> Int -> Bool
+multAssociative x y z = x * (y * z) == (x * y) * z
+
+multCommutative :: Int -> Int -> Bool
+multCommutative x y = x * y == y * x
 
 main :: IO ()
 main = do
   putStrLn "1. halfIdentity"
   quickCheck prop_halfIdentity
-
   putStrLn "2. listOrdered"
   quickCheck prop_listOrdered
+  putStrLn "3. plusAssociative"
+  quickCheck plusAssociative
+  putStrLn "3. plusCommutative"
+  quickCheck plusCommutative
+  putStrLn "4. multAssociative"
+  quickCheck multAssociative
+  putStrLn "4. multCommutative"
+  quickCheck multCommutative
 
