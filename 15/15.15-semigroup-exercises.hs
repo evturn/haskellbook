673450b8b2842f3e6817c0662f6d055e0c38de20
Evan Turner <evturn@gmail.com>
2017-11-08 13:54:46 -0500

    Start redo of semigroup chapter exercises

---
+++ 15/15.15-semigroup-exercises.hs
@@ -0,0 +1,22 @@
+import           Data.Semigroup
+import           Test.QuickCheck
+
+-----------------------------------------------------------------------------
+-- 1.
+data Trivial = Trivial
+  deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  _ <> _ = Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = return Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+main :: IO ()
+main = do
+  quickCheck (semigroupAssoc :: TrivAssoc)
