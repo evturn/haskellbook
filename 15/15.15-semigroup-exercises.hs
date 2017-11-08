018c0324c3eb2d5b7322d62eb06f8c9c7fbe085d
Evan Turner <evturn@gmail.com>
2017-11-08 14:04:31 -0500

    Add second solution for semigroup chapter exercise

--- 15/15.15-semigroup-exercises.hs
+++ 15/15.15-semigroup-exercises.hs
@@ -17,6 +17,22 @@ semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
 
 type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
 
+-----------------------------------------------------------------------------
+-- 2.
+newtype Identity a = Identity a
+  deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Identity a) where
+  Identity x <> Identity y = Identity (x <> y)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    x <- arbitrary
+    return $ Identity x
+
+type IdAssoc = Identity Trivial -> Identity Trivial -> Identity Trivial -> Bool
+
 main :: IO ()
 main = do
   quickCheck (semigroupAssoc :: TrivAssoc)
+  quickCheck (semigroupAssoc :: IdAssoc)
