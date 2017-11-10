dab67034d3afc0415641e61f9bac7a9af3cbc82f
Evan Turner <evturn@gmail.com>
2017-11-10 09:33:11 -0500

    Begin redo of third set of Functor chapter exercises

---
+++ 16/16.17-chapter-exercises-3.hs
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------------
+-- 1.
+data Quant a b = Finance
+               | Desk a
+               | Bloor b
+
+instance Functor (Quant a) where
+  fmap _ Finance   = Finance
+  fmap _ (Desk x)  = Desk x
+  fmap f (Bloor y) = Bloor (f y)
