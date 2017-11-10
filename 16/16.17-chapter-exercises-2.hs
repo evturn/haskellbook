91f5c9291c09b1cea7e4548f8a692d7f1ecc9a9e
Evan Turner <evturn@gmail.com>
2017-11-10 09:29:18 -0500

    Complete second set of Functor chapter exercises

--- 16/16.17-chapter-exercises-2.hs
+++ 16/16.17-chapter-exercises-2.hs
@@ -18,3 +18,13 @@ data Company a c b = DeepBlue a c
 instance Functor (Company e e') where
   fmap f (Something b)  = Something (f b)
   fmap _ (DeepBlue a c) = DeepBlue a c
+
+-----------------------------------------------------------------------------
+-- 3.
+data More b a = L a b a
+              | R b a b
+              deriving (Eq, Show)
+
+instance Functor (More x) where
+  fmap f (L a b a') = L (f a) b (f a')
+  fmap f (R b a b') = R b (f a) b'
