446872c338fe5fc6988906a0ea776af627b8e93d
Evan Turner <evturn@gmail.com>
2017-11-10 09:43:22 -0500

    Add third solution to third set of chapter exercises

--- 16/16.17-chapter-exercises-3.hs
+++ 16/16.17-chapter-exercises-3.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+-- Write `Functor` instances for the following datatypes.
+--
 -----------------------------------------------------------------------------
 -- 1.
 data Quant a b = Finance
@@ -8,3 +12,18 @@ instance Functor (Quant a) where
   fmap _ Finance   = Finance
   fmap _ (Desk x)  = Desk x
   fmap f (Bloor y) = Bloor (f y)
+
+-----------------------------------------------------------------------------
+-- 2.
+data K a b = K a
+
+instance Functor (K a) where
+  fmap _ (K x)  = K x
+
+-----------------------------------------------------------------------------
+-- 3.
+newtype Flip f a b = Flip (f b a)
+  deriving (Eq, Show)
+
+instance Functor (Flip K a) where
+  fmap f (Flip (K a)) = Flip $ K (f a)
