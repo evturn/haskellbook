b0c10a79232f56fcd321267560d73ff1d403eaaf
Evan Turner <evturn@gmail.com>
2017-11-10 01:44:46 -0500

    Begin redo of functor instances exercises

---
+++ 16/16.10-instances-of-func.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE ViewPatterns #-}
+
+import           Test.QuickCheck
+import           Test.QuickCheck.Function
+
+functorIdentity :: (Functor f, Eq (f a)) => f a -> Bool
+functorIdentity f = fmap id f == f
+
+functorCompose :: (Functor f, Eq (f c))
+               => (a -> b)
+               -> (b -> c)
+               -> f a
+               -> Bool
+functorCompose f g x = (fmap g (fmap f x)) == (fmap (g . f) x)
+
+functorCompose' :: (Functor f, Eq (f c))
+                => f a
+                -> Fun a b
+                -> Fun b c
+                -> Bool
+functorCompose' x (Fun _ f) (Fun _ g) =
+  (fmap (g . f) x) == (fmap g . fmap f $ x)
+
+type IntToInt = Fun Int Int
+type IntFC = [Int] -> IntToInt -> IntToInt -> Bool
+
+-----------------------------------------------------------------------------
+-- 1.
+newtype Identity a = Identity a
+  deriving (Eq, Show)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    x <- arbitrary
+    return $ Identity x
+
+type IdId = Identity Int -> Bool
+type IdFC = Identity Int -> IntToInt -> IntToInt -> Bool
+
+
+main :: IO ()
+main = do
+  quickCheck (functorIdentity :: IdId)
+  quickCheck (functorCompose' :: IdFC)
