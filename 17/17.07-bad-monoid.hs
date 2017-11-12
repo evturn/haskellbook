1d00cc1b0fcda99de7f89d7ee6d260c58b44c709
Evan Turner <evturn@gmail.com>
2017-11-12 04:14:50 -0500

    Redo bad monoid example for chapter 17

---
+++ 17/17.07-bad-monoid.hs
@@ -0,0 +1,33 @@
+module BadMonoid where
+
+import           Data.Monoid
+import           Test.QuickCheck
+import           Test.QuickCheck.Checkers
+import           Test.QuickCheck.Classes
+
+data Bull = Fools
+          | Twoo
+          deriving (Eq, Show)
+
+instance Arbitrary Bull where
+  arbitrary = frequency [ (1, return Fools)
+                        , (1, return Twoo)
+                        ]
+
+instance Monoid Bull where
+  mempty      = Fools
+  mappend _ _ = Fools
+
+instance EqProp Bull where
+  (=-=) = eq
+
+type SSI = (String, String, Int)
+
+trigger :: [SSI]
+trigger = undefined
+
+main :: IO ()
+main = do
+  quickBatch $ monoid      Twoo
+  quickBatch $ applicative trigger
+
