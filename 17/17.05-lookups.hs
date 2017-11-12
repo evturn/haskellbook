60d73766df799662d1b5bd03eb6a65cc6ae38501
Evan Turner <evturn@gmail.com>
2017-11-11 20:15:19 -0500

    Complete redo of lookups exercise for Applicative

--- 17/17.05-lookups.hs
+++ 17/17.05-lookups.hs
@@ -34,3 +34,17 @@ max' = max
 
 maxed :: Maybe Int
 maxed = max' <$> x <*> y'
+
+-----------------------------------------------------------------------------
+-- 4.
+xs = [1, 2, 3]
+ys = [4, 5, 6]
+
+x' :: Maybe Integer
+x' = lookup 3 $ zip xs ys
+
+y'' :: Maybe Integer
+y'' = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = fmap sum $ (,) <$> x' <*> y''
