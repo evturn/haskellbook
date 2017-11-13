b207c3f6ffa70ae29d14ba4783dc474aece226de
Evan Turner <evturn@gmail.com>
2017-11-13 09:57:02 -0500

    Add Pair Applicative instance to chapter exercise

--- 17/17.09-chapter-exercises-2.hs
+++ 17/17.09-chapter-exercises-2.hs
@@ -27,10 +27,36 @@ instance Eq a => EqProp (Pair a) where
 pair :: Pair SSI
 pair = undefined
 
+-----------------------------------------------------------------------------
+-- 2.
+data Two a b = Two a b
+  deriving (Eq, Show)
 
+instance Functor (Two a) where
+  fmap f (Two x y) = Two x (f y)
+
+instance Monoid a => Applicative (Two a) where
+  pure x               = Two mempty x
+  Two x f <*> Two x' y = Two (x `mappend` x') (f y)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    return $ Two x y
+
+instance (Eq a, Eq b) => EqProp (Two a b) where
+  (=-=) = eq
+
+two :: Two String SSI
+two = undefined
 
 -----------------------------------------------------------------------------
 main :: IO ()
 main = do
+  putStrLn "\n1. Pair"
   quickBatch $ functor     pair
   quickBatch $ applicative pair
+  putStrLn "\n2. Two"
+  quickBatch $ functor     two
+  quickBatch $ applicative two
