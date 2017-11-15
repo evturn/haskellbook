7e8377e64f352c9e37713ec79cc93f59ec4ff59a
Evan Turner <evturn@gmail.com>
2017-11-14 22:17:29 -0500

    Add second solution to Monad instance exercises

--- 18/18.07-chapter-exercises-1.hs
+++ 18/18.07-chapter-exercises-1.hs
@@ -28,8 +28,48 @@ instance Eq a => EqProp (Nope a) where
 
 nope = undefined :: Nope SSI
 
+-----------------------------------------------------------------------------
+-- 2.
+data PhhhbbtttEither b a = Left' a
+                         | Right' b
+                         deriving (Eq, Show)
+
+instance Functor (PhhhbbtttEither b) where
+  fmap f (Left' x)  = Left' (f x)
+  fmap _ (Right' x) = Right' x
+
+instance Applicative (PhhhbbtttEither b) where
+  pure = Left'
+  Left' f  <*> Left' y  = Left' (f y)
+  Right' x <*> _        = Right' x
+  _        <*> Right' y = Right' y
+
+instance Monad (PhhhbbtttEither b) where
+  return = pure
+  Left' x  >>= f = f x
+  Right' x >>= _ = Right' x
+
+instance (Arbitrary b, Arbitrary a) => Arbitrary (PhhhbbtttEither b a) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    elements [ Left' y
+             , Right' x
+             ]
+
+instance (Eq b, Eq a) => EqProp (PhhhbbtttEither b a) where
+  (=-=) = eq
+
+peither = undefined :: PhhhbbtttEither String SSI
+
+
 main :: IO ()
 main = do
+  putStrLn "\n1. Nope"
   quickBatch $ functor     nope
   quickBatch $ applicative nope
   quickBatch $ monad       nope
+  putStrLn "\n2. PhhhbbtttEither"
+  quickBatch $ functor     peither
+  quickBatch $ applicative peither
+  quickBatch $ monad       peither
