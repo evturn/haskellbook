36f687bf17638847d6857e133bb9bf300042a1ab
Evan Turner <evturn@gmail.com>
2017-11-16 18:28:28 -0500

    Add Traversable instance for Constant datatype

--- 21/21.12-traversable-instances.hs
+++ 21/21.12-traversable-instances.hs
@@ -30,7 +30,38 @@ instance Eq a => EqProp (Identity a) where
 identityTraversable :: Identity IIIs
 identityTraversable = undefined
 
+-----------------------------------------------------------------------------
+-- Constant
+--
+newtype Constant a b = Constant
+                     { getConstant :: a }
+                     deriving (Eq, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant y) = Constant y
+
+instance Foldable (Constant a) where
+  foldr _ z _ = z
+
+instance Traversable (Constant a) where
+  traverse f (Constant x) = pure $ Constant x
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Constant a b) where
+  arbitrary = do
+    x <- arbitrary
+    return $ Constant x
+
+instance (Eq a, Eq b) => EqProp (Constant a b) where
+  (=-=) = eq
+
+constantTraversable :: Constant Int IIIs
+constantTraversable = undefined
 
 main :: IO ()
 main = do
+  putStrLn "\n-- Identity"
+  quickBatch $ functor     identityTraversable
   quickBatch $ traversable identityTraversable
+  putStrLn "\n-- Constant"
+  quickBatch $ functor     constantTraversable
+  quickBatch $ traversable constantTraversable
