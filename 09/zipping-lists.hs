2d379054063e1e06bdda8083bbe8ed712e848b0e
Evan Turner <evturn@gmail.com>
2017-11-23 07:53:14 -0800

    Complete zipping exercises

--- 09/zipping-lists.hs
+++
@@ -1,8 +0,0 @@
-
-zip' _ []          = []
-zip' [] _          = []
-zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
-
-zipWith' _ _ []          = []
-zipWith' _ [] _          = []
-zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
