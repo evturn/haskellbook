496e84a9576ddc5b3e4e348199e23ef64d170e39
Evan Turner <evturn@gmail.com>
2017-11-24 13:00:06 -0500

    Begin first chapter exercise on Reader

---
+++ 22/22.11-reader-practice.hs
@@ -0,0 +1,50 @@
+module ReaderPractice where
+
+import           Control.Applicative
+import           Data.Maybe
+
+x :: [Integer]
+x = [1, 2, 3]
+
+y :: [Integer]
+y = [4, 5, 6]
+
+z :: [Integer]
+z = [7, 8, 9]
+
+xs :: Maybe Integer
+xs = lookup 3 $ zip x y
+
+ys :: Maybe Integer
+ys = lookup 6 $ zip y z
+
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+z' :: Integer -> Maybe Integer
+z' n = lookup n $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 = (,) <$> z' <*> z'
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
+
+bolt :: Integer -> Bool
+bolt = (&&) <$> (>3) <*> (<8)
+
+main :: IO ()
+main = do
+  print $ sequenceA [Just 3, Just 2, Just 1]
+  print $ sequenceA [x, y]
+  print $ sequenceA [xs, ys]
+  print $ summed <$> ((,) <$> xs <*> ys)
+  print $ fmap summed ((,) <$> xs <*> zs)
+  print $ bolt 7
+  print $ fmap bolt z
