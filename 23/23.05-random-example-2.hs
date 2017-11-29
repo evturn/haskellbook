32df84153bfd9fc391ec4d6bee23e8fe37816a33
Evan Turner <evturn@gmail.com>
2017-11-29 06:16:56 -0500

    Complete random number example using State and Random

--- 23/23.05-random-example-2.hs
+++ 23/23.05-random-example-2.hs
@@ -47,3 +47,14 @@ infiniteDie = repeat <$> rollDie
 
 nDie :: Int -> State StdGen [Die]
 nDie n = replicateM n rollDie
+
+rollsToGetTwenty :: StdGen -> Int
+rollsToGetTwenty g = go 0 0 g
+  where
+    go :: Int -> Int -> StdGen -> Int
+    go sum count gen
+      | sum >= 20 = count
+      | otherwise = let (die, nextGen) = randomR (1, 6) gen
+                     in go (sum + die) (count + 1) nextGen
+
+
