f3926feb419132dacd6ffe063880d0e2a4f222a8
Evan Turner <evturn@gmail.com>
2017-12-03 11:01:15 -0500

    Create module for parsing QuasiQuotes

---
+++ 24/24.06-quasimodo.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Quasimodo where
+
+import           Control.Applicative
+import           Text.RawString.QQ
+import           Text.Trifecta
+
+type NumberOrString = Either Integer String
+
+parseNos :: Parser NumberOrString
+parseNos = skipMany (oneOf "\n")
+           >>  (Left <$> integer)
+           <|> (Right <$> some letter)
+
+eitherOr :: String
+eitherOr = [r|
+123
+abc
+456
+def
+|]
+
+main :: IO ()
+main = do
+  let p f i = parseString f mempty i
+  print $ p parseNos eitherOr
