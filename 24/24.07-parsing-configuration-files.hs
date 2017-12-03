ff50302ae27d5d506b03f4a80a89ab3b67dd6235
Evan Turner <evturn@gmail.com>
2017-12-03 16:26:35 -0500

    Add parsers for config file

---
+++ 24/24.07-parsing-configuration-files.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+
+module Data.Ini where
+
+import           Control.Applicative
+import           Data.ByteString     (ByteString)
+import           Data.Char           (isAlpha)
+import           Data.Map            (Map)
+import qualified Data.Map            as M
+import           Data.Text           (Text)
+import qualified Data.Text.IO        as TIO
+import           Test.Hspec
+import           Text.RawString.QQ
+import           Text.Trifecta
+
+headerEx :: ByteString
+headerEx = "[blah]"
+
+newtype Header = Header String
+  deriving (Eq, Ord, Show)
+
+parseBracketPair :: Parser a -> Parser a
+parseBracketPair p = char '[' *> p <* char ']'
+
+parseHeader :: Parser Header
+parseHeader = parseBracketPair (Header <$> some letter)
+
+assignmentEx :: ByteString
+assignmentEx = "woot=1"
+
+type Name        = String
+type Value       = String
+type Assignments = Map Name Value
+
+parseAssignment :: Parser (Name, Value)
+parseAssignment = do
+  name <- some letter
+  _    <- char '='
+  val  <- some (noneOf "\n")
+  skipEOL
+  return (name, val)
+
+skipEOL :: Parser ()
+skipEOL = skipMany (oneOf "\n")
+
+commentEx :: ByteString
+commentEx = "; last modified April\
+            \ 2001 by John Doe"
+
+commentEx' :: ByteString
+commentEx' = "; blah\n; woot\n \n;hah"
+
+skipComments :: Parser ()
+skipComments = skipMany $ do
+  _ <- char ';' <|> char '#'
+  skipMany (noneOf "\n")
+  skipEOL
+
+sectionEx :: ByteString
+sectionEx = "; ignore me\n[states]\nChris=Texas"
+
+sectionEx' :: ByteString
+sectionEx' = [r|
+; ignore me
+[states]
+Chris=Texas
+|]
+
+sectionEx'' :: ByteString
+sectionEx'' = [r|
+;comment
+[section]
+host=wikipedia.org
+alias=claw
+
+[whatisit]
+red=intoothandclaw
+|]
+
+data Section = Section Header Assignments
+  deriving (Eq, Show)
+
+newtype Config = Config (Map Header Assignments)
+  deriving (Eq, Show)
+
+skipWhitespace :: Parser ()
+skipWhitespace = skipMany (char ' ' <|> char '\n')
+
+parseSection :: Parser Section
+parseSection = do
+  skipWhitespace
+  skipComments
+  h <- parseHeader
+  skipEOL
+  assignments <- some parseAssignment
+  return $ Section h (M.fromList assignments)
