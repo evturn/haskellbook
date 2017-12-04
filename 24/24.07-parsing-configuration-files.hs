4bd74cb2c611012d6a98532fc2bc50bdf0d370a6
Evan Turner <evturn@gmail.com>
2017-12-04 01:26:44 -0500

    Create parser for config files

--- 24/24.07-parsing-configuration-files.hs
+++ 24/24.07-parsing-configuration-files.hs
@@ -95,3 +95,67 @@ parseSection = do
   skipEOL
   assignments <- some parseAssignment
   return $ Section h (M.fromList assignments)
+
+rollup :: Section -> Map Header Assignments -> Map Header Assignments
+rollup (Section h a) m = M.insert h a m
+
+parseIni :: Parser Config
+parseIni = do
+  sections <- some parseSection
+  let mapOfSections = foldr rollup M.empty sections
+  return (Config mapOfSections)
+
+maybeSuccess :: Result a -> Maybe a
+maybeSuccess (Success a) = Just a
+maybeSuccess _           = Nothing
+
+main :: IO ()
+main = hspec $ do
+
+  describe "Assignment Parsing" $
+    it "can parse a simple assignment" $ do
+      let m  = parseByteString parseAssignment mempty assignmentEx
+          r' = maybeSuccess m
+      print m
+      r' `shouldBe` Just ("woot", "1")
+
+  describe "Header Parsing" $
+    it "can parse a simple header" $ do
+      let m  = parseByteString parseHeader mempty headerEx
+          r' = maybeSuccess m
+      print m
+      r' `shouldBe` Just (Header "blah")
+
+  describe "Comment parsing" $
+    it "Skips comment before header" $ do
+      let p  = skipComments >> parseHeader
+          i  = "; woot\n[blah]"
+          m  = parseByteString p mempty i
+          r' = maybeSuccess m
+      print m
+      r' `shouldBe` Just (Header "blah")
+
+  describe "Sections parsing" $
+    it "Can parse a simple section" $ do
+      let m         = parseByteString parseSection mempty sectionEx
+          r'        = maybeSuccess m
+          states    = M.fromList [("Chris", "Texas")]
+          expected' = Just (Section (Header "states") states)
+      print m
+      r' `shouldBe` expected'
+
+  describe "INI parsing" $
+    it "Can parse multiple sections" $ do
+      let m  = parseByteString parseIni mempty sectionEx''
+          r' = maybeSuccess m
+          sectionValues = M.fromList [ ("alias", "claw")
+                                     , ("host", "wikipedia.org")
+                                     ]
+          whatisitValues = M.fromList [("red", "intoothandclaw")]
+          expected' = Just (Config (M.fromList [ (Header "section"
+                                               , sectionValues)
+                                               , (Header "whatisit"
+                                               , whatisitValues)
+                                               ]))
+      print m
+      r' `shouldBe` expected'
