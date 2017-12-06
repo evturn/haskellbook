1bc49d3bb8adb84390561977235149434b920a8d
Evan Turner <evturn@gmail.com>
2017-12-06 02:39:15 -0500

    Complete marshalling example using aeson

--- 24/24.10-marshalling.hs
+++ 24/24.10-marshalling.hs
@@ -6,6 +6,7 @@ module Marshalling where
 import           Control.Applicative
 import           Data.Aeson
 import           Data.ByteString.Lazy (ByteString)
+import           Data.Scientific      (floatingOrInteger)
 import           Data.Text            (Text)
 import qualified Data.Text            as T
 import           Text.RawString.QQ
@@ -46,7 +47,23 @@ instance FromJSON Color where
                          (Yellow <$> v .: "yellow")
   parseJSON _          = fail "Expected an object for Color"
 
+data NumberOrString = Numba Integer
+                    | Stringy Text
+                    deriving (Eq, Show)
+
+instance FromJSON NumberOrString where
+  parseJSON (Number i) = case floatingOrInteger i of
+                           (Left _)        -> fail "Must be integral number"
+                           (Right integer) -> return $ Numba integer
+  parseJSON (String s) = return $ Stringy s
+  parseJSON _          = fail "NumberOrString must be a number or string"
+
+dec :: ByteString -> Maybe NumberOrString
+dec = decode
+
+eitherDec :: ByteString -> Either String NumberOrString
+eitherDec = eitherDecode
+
 main = do
-  let d :: Maybe TestData
-      d = decode sectionJson
-  print d
+  print $ dec "blah"
+  print $ eitherDec "blah"
