384890c0e94de6a64a342ea6b243979eb4fb7c8b
Evan Turner <ev@evturn.com>
2017-12-09 01:35:06 -0500

    Create Foldable instance for Compose newtype

---
+++ 25/25.06-compose-instances.hs
@@ -0,0 +1,18 @@
+import           Control.Applicative
+
+newtype Compose f g a = Compose
+    { getCompose :: f (g a)
+    } deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose x) = Compose $ (fmap . fmap) f x
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure x = Compose $ (pure . pure) x
+  Compose f <*> Compose x = Compose (liftA2 (<*>) f x)
+
+-----------------------------------------------------------------------------
+-- 1.
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldMap f (Compose x) = (foldMap . foldMap) f x
+
