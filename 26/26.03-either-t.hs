d5fd4c88af69ff0587737fc9fd20074a63ff0d2b
Evan Turner <ev@evturn.com>
2017-12-09 22:06:02 -0500

    Add Applicative instance for EitherT type

--- 26/26.03-either-t.hs
+++ 26/26.03-either-t.hs
@@ -5,3 +5,9 @@ newtype EitherT e m a = EitherT
 -- 1.
 instance Functor m => Functor (EitherT e m) where
   fmap f (EitherT x) = EitherT $ (fmap . fmap) f x
+
+-----------------------------------------------------------------------------
+-- 2.
+instance Applicative m => Applicative (EitherT e m) where
+  pure x = EitherT (pure (pure x))
+  EitherT f <*> EitherT x = EitherT $ (<*>) <$> f <*> x
