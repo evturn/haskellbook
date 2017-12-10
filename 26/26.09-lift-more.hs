1d24fd3a36ef0373c6c534faba61797f13109729
Evan Turner <ev@evturn.com>
2017-12-10 09:16:18 -0500

    Complete MonadTrans lift implementations exercise

--- 26/26.09-lift-more.hs
+++ 26/26.09-lift-more.hs
@@ -7,3 +7,13 @@ newtype EitherT e m a = EitherT
 
 instance MonadTrans (EitherT e) where
   lift = EitherT . fmap Right
+
+-----------------------------------------------------------------------------
+-- 2.
+newtype StateT s m a = StateT
+  { runStateT :: s -> m (a, s) }
+
+instance MonadTrans (StateT s) where
+  lift m = StateT $ \s -> do
+    a <- m
+    return (a, s)
