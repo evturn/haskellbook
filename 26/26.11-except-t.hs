b005e265b2d86318b40bbd14a6d1bab95107ff4a
Evan Turner <ev@evturn.com>
2017-12-10 18:27:26 -0500

    Finish ExceptT example using Scotty web framework

--- 26/26.11-except-t.hs
+++ 26/26.11-except-t.hs
@@ -3,21 +3,36 @@
 module Main where
 
 import           Control.Monad.IO.Class
-import           Data.Text.Lazy         (Text)
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.Except
+import           Data.Text.Lazy             (Text)
+import qualified Data.Text.Lazy             as TL
 import           Web.Scotty
 
-param' :: Parsable a => Text -> ActionM (Either String a)
-param' k = rescue
+param' :: Parsable a => Text -> ExceptT String ActionM a
+param' k = ExceptT $ rescue
            (Right <$> param k)
            (const (return (Left $ "The key: " ++ show k ++ " was missing!")))
 
+type Reco = (Integer, Integer, Integer, Integer)
+
+tshow :: Reco -> Text
+tshow = TL.pack . show
+
+main :: IO ()
 main = scotty 3000 $ do
-  get "/:word" $ do
-    beam <- param "word"
-    a <- param' "1"
-    let a' = either (const 0) id a
-    liftIO $ print (a :: Either String Int)
-    liftIO $ print (a' :: Int)
-    html $ mconcat [ "<h1>Scotty, "
-                   , beam
-                   , " me up!</h1>"]
+  get "/" $ do
+    reco <- runExceptT $ do
+      a <- param' "1"
+      liftIO $ print a
+      b <- param' "2"
+      c <- param' "3"
+      d <- param' "4"
+      (lift . lift) $ print b
+      return ((a, b, c, d) :: Reco)
+    case reco of
+      Left e  -> text (TL.pack e)
+      Right r -> html $ mconcat [ "<h1>Success! Reco was: "
+                                , tshow r
+                                , "</h1>"
+                                ]
