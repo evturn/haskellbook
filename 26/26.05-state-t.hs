ce2bd5429304947a61e2b39db624e209c18d9989
Evan Turner <ev@evturn.com>
2017-12-10 01:05:03 -0500

    Write Applicative instance for StateT newtype

--- 26/26.05-state-t.hs
+++ 26/26.05-state-t.hs
@@ -5,3 +5,13 @@ newtype StateT s m a = StateT
 -- 1.
 instance Functor m => Functor (StateT s m) where
   fmap f (StateT m) = StateT $ \s -> fmap (\(x, s') -> (f x, s')) (m s)
+
+-----------------------------------------------------------------------------
+-- 2.
+instance Monad m => Applicative (StateT s m) where
+  pure x = StateT $ \s -> pure (x, s)
+  StateT mf <*> StateT mx = StateT $ \s -> do
+    (f, s') <- mf s
+    (x, s'') <- mx s'
+    return (f x, s'')
+
