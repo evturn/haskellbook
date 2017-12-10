c2b66c1ce1370eb472b3d3fbc3827da3d83d1db6
Evan Turner <ev@evturn.com>
2017-12-10 10:57:26 -0500

    Complete MonadIO instances exercise

--- 26/26.10-some-instances.hs
+++ 26/26.10-some-instances.hs
@@ -50,3 +50,33 @@ instance MonadTrans (ReaderT r) where
 
 instance MonadIO m => MonadIO (ReaderT r m) where
   liftIO = lift . liftIO
+
+-----------------------------------------------------------------------------
+-- 3.
+
+newtype StateT s m a = StateT
+  { runStateT :: s -> m (a, s) }
+
+instance Functor m => Functor (StateT s m) where
+  fmap f (StateT m) = StateT $ \s -> fmap (\(x, s') -> (f x, s')) (m s)
+
+instance Monad m => Applicative (StateT s m) where
+  pure x = StateT $ \s -> pure (x, s)
+  StateT mf <*> StateT mx = StateT $ \s -> do
+    (f, s') <- mf s
+    (x, s'') <- mx s'
+    return (f x, s'')
+
+instance Monad m => Monad (StateT s m) where
+  return = pure
+  StateT m >>= f = StateT $ \s -> do
+    (x, s') <- m s
+    runStateT (f x) s'
+
+instance MonadTrans (StateT s) where
+  lift m = StateT $ \s -> do
+    a <- m
+    return (a, s)
+
+instance MonadIO m => MonadIO (StateT s m) where
+  liftIO = lift . liftIO
