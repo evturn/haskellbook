a09f16e39500579797e321f3c086b6900c676484
Evan Turner <ev@evturn.com>
2017-12-10 10:53:04 -0500

    Add MonadIO instance for ReaderT newtype

--- 26/26.10-some-instances.hs
+++ 26/26.10-some-instances.hs
@@ -24,5 +24,29 @@ instance Monad m => Monad (MaybeT m) where
 instance MonadTrans MaybeT where
   lift = MaybeT . fmap Just
 
-instance (MonadIO m) => MonadIO (MaybeT m) where
+instance MonadIO m => MonadIO (MaybeT m) where
+  liftIO = lift . liftIO
+
+-----------------------------------------------------------------------------
+-- 2.
+newtype ReaderT r m a = ReaderT
+  { runReaderT :: r -> m a }
+
+instance Functor m => Functor (ReaderT r m) where
+  fmap f (ReaderT x) = ReaderT $ (fmap . fmap) f x
+
+instance Applicative m => Applicative (ReaderT r m) where
+  pure x = ReaderT $ (pure (pure x))
+  ReaderT f <*> ReaderT x = ReaderT $ (<*>) <$> f <*> x
+
+instance Monad m => Monad (ReaderT r m) where
+  return = pure
+  ReaderT m >>= f = ReaderT $ \r -> do
+    x <- m r
+    runReaderT (f x) r
+
+instance MonadTrans (ReaderT r) where
+  lift = ReaderT . const
+
+instance MonadIO m => MonadIO (ReaderT r m) where
   liftIO = lift . liftIO
