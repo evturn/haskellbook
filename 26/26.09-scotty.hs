c03e7b23ead394f1c90811dcd90c910052d6524b
Evan Turner <ev@evturn.com>
2017-12-10 05:53:15 -0500

    Embed IO expression in ActionT context

--- 26/26.09-scotty.hs
+++ 26/26.09-scotty.hs
@@ -3,14 +3,22 @@
 module Scotty where
 
 import           Control.Monad.Trans.Class
-import           Data.Monoid               (mconcat)
+import           Control.Monad.Trans.Except
+import           Control.Monad.Trans.Reader
+import           Control.Monad.Trans.State.Lazy hiding (get)
+import           Data.Monoid                    (mconcat)
 import           Web.Scotty
-import           Web.Scotty.Internal.Types (ActionT (..))
+import           Web.Scotty.Internal.Types      (ActionT (..))
 
 main = scotty 3000 $ do
   get "/:word" $ do
     beam <- param "word"
-    ActionT . lift . lift .lift $ putStrLn "hello"
+    (ActionT
+     . (ExceptT . fmap Right)
+     . ReaderT . const
+     . \m -> StateT (\s -> do
+                         a <- m
+                         return (a, s))) (putStrLn "hello")
     html $ mconcat [ "<h1>Scotty, "
                    , beam
                    , " me up!</h1>"
