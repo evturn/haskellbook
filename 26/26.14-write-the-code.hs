3723563858cb38b0b38ac808d2edb2c2b2dc6586
Evan Turner <ev@evturn.com>
2017-12-10 19:08:18 -0500

    Create pointfree version of first solution

--- 26/26.14-write-the-code.hs
+++ 26/26.14-write-the-code.hs
@@ -4,3 +4,9 @@ import           Control.Monad.Trans.Reader
 -- 1.
 rDec :: Num a => Reader a a
 rDec = ReaderT $ return . \x -> x - 1
+
+-----------------------------------------------------------------------------
+-- 2.
+rDec' :: Num a => Reader a a
+rDec' = ReaderT $ return . ((+) (-1))
+
