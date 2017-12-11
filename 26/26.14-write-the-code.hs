bfe50d3820c4cd4c04f769baa9ff258e0b6a62df
Evan Turner <ev@evturn.com>
2017-12-10 19:43:50 -0500

    Print value then return incremented value in ReaderT

--- 26/26.14-write-the-code.hs
+++ 26/26.14-write-the-code.hs
@@ -12,7 +12,14 @@ rDec' :: Num a => Reader a a
 rDec' = ReaderT $ return . ((+) (-1))
 
 -----------------------------------------------------------------------------
-  -- 3 & 4.
+-- 3 & 4.
 rShow :: Show a => ReaderT a Identity String
 rShow = ReaderT $ return . show
+
+-----------------------------------------------------------------------------
+-- 5.
+rPrintAndInc :: (Num a, Show a) => ReaderT a IO a
+rPrintAndInc = ReaderT $ \r -> do
+  putStrLn ("Hi: " ++ show r)
+  return $ r + 1
 
