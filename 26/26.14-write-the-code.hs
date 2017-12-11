d965747db759e516a4ef909de484bfc39a04d1cf
Evan Turner <ev@evturn.com>
2017-12-10 19:14:07 -0500

    Add solutions 3 and 4 to chapter 26 exercises on Reader

--- 26/26.14-write-the-code.hs
+++ 26/26.14-write-the-code.hs
@@ -1,4 +1,5 @@
 import           Control.Monad.Trans.Reader
+import           Data.Functor.Identity
 
 -----------------------------------------------------------------------------
 -- 1.
@@ -9,4 +10,9 @@ rDec = ReaderT $ return . \x -> x - 1
 -- 2.
 rDec' :: Num a => Reader a a
 rDec' = ReaderT $ return . ((+) (-1))
+
+-----------------------------------------------------------------------------
+  -- 3 & 4.
+rShow :: Show a => ReaderT a Identity String
+rShow = ReaderT $ return . show
 
