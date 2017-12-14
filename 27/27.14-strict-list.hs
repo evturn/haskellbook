0398957885194e91093f6579aba03f8ad25f2f9c
Evan Turner <ev@evturn.com>
2017-12-13 22:23:22 -0500

    Add strict implementation of list

---
+++ 27/27.14-strict-list.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE Strict #-}
+
+module StrictList where
+
+data List a = Nil
+            | Cons a (List a)
+            deriving Show
+
+take' n _           | n <= 0 = Nil
+take' _ Nil         = Nil
+take' n (Cons x xs) = (Cons x (take' (n - 1) xs))
+
+map' _ Nil         = Nil
+map' f (Cons x xs) = (Cons (f x) (map' f xs))
+
+repeat' x = xs where xs = (Cons x xs)
+
+main = do
+  print $ take' 10 $ map' (+1) (repeat' 1)
