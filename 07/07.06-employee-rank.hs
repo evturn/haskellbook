b8a23c24607ccc52182cfd3ff258e91a56eea78d
Evan Turner <ev@evturn.com>
2017-12-27 03:55:00 -0800

    Redo example on higher order functions

--- 07/07.06-employee-rank.hs
+++ 07/07.06-employee-rank.hs
@@ -7,16 +7,16 @@ data Employee = Coder
 reportBoss :: Employee -> Employee -> IO ()
 reportBoss e e' = putStrLn $ show e ++ " is the boss of " ++ show e'
 
-coderz5000 :: Employee -> Employee -> Ordering
-coderz5000 Coder Coder = EQ
-coderz5000 Coder _     = GT
-coderz5000 _     Coder = LT
-coderz5000 e e'        = compare e e'
+codersRuleCEOsDrool :: Employee -> Employee -> Ordering
+codersRuleCEOsDrool Coder Coder = EQ
+codersRuleCEOsDrool Coder _     = GT
+codersRuleCEOsDrool _     Coder = LT
+codersRuleCEOsDrool e     e'    = compare e e'
 
 employeeRank :: (Employee -> Employee -> Ordering)
-              -> Employee
-              -> Employee
-              -> IO ()
+             -> Employee
+             -> Employee
+             -> IO ()
 employeeRank f e e' = case f e e' of
   GT -> reportBoss e e'
   EQ -> putStrLn "Neither employee is the boss"
