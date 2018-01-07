c16c981b661fc66a18d5adece346adb7f7bf3da0
Evan Turner <ev@evturn.com>
2018-01-06 23:31:19 -0500

    Add example of IO with MVar

---
+++ 29/29.04-what-happens.hs
@@ -0,0 +1,14 @@
+module WhatHappens where
+
+import           Control.Concurrent
+
+myData :: IO (MVar Int)
+myData = newEmptyMVar
+
+main :: IO ()
+main = do
+  mv   <- myData
+  putMVar mv 0
+  -- mv'  <- myData
+  zero <- takeMVar mv
+  print zero
