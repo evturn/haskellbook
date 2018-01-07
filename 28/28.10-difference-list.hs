667517efa972c45fcd12c047cc26516ad29ddc93
Evan Turner <ev@evturn.com>
2018-01-06 19:47:26 -0500

    Add benchmark for liss and difference lists

--- 28/28.10-difference-list.hs
+++ 28/28.10-difference-list.hs
@@ -1,3 +1,5 @@
+import           Criterion.Main
+
 newtype DList a = DL { unDL :: [a] -> [a] }
 
 -----------------------------------------------------------------------------
@@ -37,3 +39,23 @@ snoc xs x = DL ((++  [x]) . unDL xs)
 append :: DList a -> DList a -> DList a
 append xs ys = DL (unDL xs . unDL ys)
 {-# INLINE append #-}
+
+schlemiel :: Int -> [Int]
+schlemiel i = go i []
+  where
+    go 0 xs = xs
+    go n xs = go (n-1) ([n] ++ xs)
+
+constructDlist :: Int -> [Int]
+constructDlist i = toList $ go i empty
+  where
+    go 0 xs = xs
+    go n xs = go (n-1) (singleton n `append` xs)
+
+main :: IO ()
+main = defaultMain
+  [ bench "concat list" $
+    whnf schlemiel 123456
+  , bench "concat dlist" $
+    whnf constructDlist 123456
+  ]
