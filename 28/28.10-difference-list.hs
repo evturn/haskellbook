5811997fd2f09bb20bb6f5bf1bda4fca9622ea26
Evan Turner <ev@evturn.com>
2018-01-06 19:37:32 -0500

    Add infix solutions for Difference List exercise

--- 28/28.10-difference-list.hs
+++ 28/28.10-difference-list.hs
@@ -11,3 +11,23 @@ empty = DL $ \_ -> []
 singleton :: a -> DList a
 singleton x = DL $ \_ -> [x]
 {-# INLINE singleton #-}
+
+-----------------------------------------------------------------------------
+-- 3.
+toList :: DList a -> [a]
+toList d = (unDL d) []
+{-# INLINE toList #-}
+
+-----------------------------------------------------------------------------
+-- 4.
+infixr `cons`
+cons :: a -> DList a -> DList a
+cons x xs = DL ((x:) . unDL xs)
+{-# INLINE cons #-}
+
+-----------------------------------------------------------------------------
+-- 5.
+infixl `snoc`
+snoc :: DList a -> a -> DList a
+snoc xs x = DL ((++ [x]) . unDL xs)
+{-# INLINE snoc #-}
