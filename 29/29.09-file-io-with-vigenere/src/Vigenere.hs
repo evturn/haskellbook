6c66b032e6b81dd056eec5d8e16e57e18e071fde
Evan Turner <ev@evturn.com>
2018-01-07 01:30:21 -0500

    Wait for user input before timing out

--- 29/29.09-file-io-with-vigenere/src/Vigenere.hs
+++ 29/29.09-file-io-with-vigenere/src/Vigenere.hs
@@ -2,7 +2,9 @@ module Vigenere where
 
 import           Cipher
 import           System.Environment (getArgs)
-import           System.IO          (hGetLine, hPutStr, stdin, stdout)
+import           System.Exit
+import           System.IO          (hGetLine, hPutStr, hWaitForInput, stderr,
+                                     stdin, stdout)
 
 readIn :: String -> Int
 readIn x = read x
@@ -11,10 +13,18 @@ parseArgs :: Int -> String -> String -> String
 parseArgs k "-d" xs = uncaesar k xs
 parseArgs k _    xs = caesar k xs
 
+finishWithError :: IO a
+finishWithError = do
+  hPutStr stderr "Timed out"
+  exitWith (ExitFailure 1)
+
 run :: IO ()
 run = do
   (k:m:[]) <- getArgs
   putStrLn "Start typing."
-  input <- hGetLine stdin
+  success <- hWaitForInput stdin (5000)
+  input <- case success of
+    False -> finishWithError
+    True  -> hGetLine stdin
   putStrLn $ parseArgs (readIn k) m input
 
