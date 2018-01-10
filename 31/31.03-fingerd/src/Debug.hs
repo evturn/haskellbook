46d86abd3d0fe5358e72771088723441bb0f1d7f
Evan Turner <ev@evturn.com>
2018-01-09 19:31:13 -0500

    Create TCP server

--- 31/31.03-fingerd/src/Debug.hs
+++ 31/31.03-fingerd/src/Debug.hs
@@ -1,3 +1,28 @@
-module Debug where
+module Main where
 
--- something something print to screen.
+import           Control.Monad             (forever)
+import           Network.Socket            hiding (recv)
+import           Network.Socket.ByteString (recv, sendAll)
+
+logAndEcho :: Socket -> IO ()
+logAndEcho sock = forever $ do
+  (soc, _) <- accept sock
+  printAndKickback soc
+  close soc
+    where
+      printAndKickback conn = do
+        msg <- recv conn 1024
+        print msg
+        sendAll conn msg
+
+main :: IO ()
+main = withSocketsDo $ do
+  addrinfos <- getAddrInfo
+               (Just (defaultHints { addrFlags = [AI_PASSIVE] }))
+               Nothing (Just "79")
+  let serveraddr = head addrinfos
+  sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+  bind sock (addrAddress serveraddr)
+  listen sock 1
+  logAndEcho sock
+  close sock
