0496d4c18947e667e64379157905ae33969d179c
Evan Turner <ev@evturn.com>
2018-01-09 19:55:50 -0500

    Add main imports to project

--- 31/31.03-fingerd/src/Main.hs
+++ 31/31.03-fingerd/src/Main.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Main where
+
+import           Control.Exception
+import           Control.Monad                (forever)
+import           Data.ByteString              (ByteString)
+import qualified Data.ByteString              as BS
+import           Data.List                    (intersperse)
+import           Data.Text                    (Text)
+import qualified Data.Text                    as T
+import           Data.Text.Encoding           (decodeUtf8, encodeUtf8)
+import           Data.Typeable
+import qualified Database.SQList.Simple       as SQLite
+import           Database.SQLite.Simple       hiding (close)
+import           Database.SQLite.Simple.Types
+import           Network.Socket               hiding (close, recv)
+import           Network.Socket.ByteString    (recv, sendAll)
+import           Text.RawString.QQ
+
+
 
 main :: IO ()
 main = do
