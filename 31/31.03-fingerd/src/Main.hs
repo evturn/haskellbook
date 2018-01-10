4a464294caeb9a06f33367fd9e7c732d9f521f51
Evan Turner <ev@evturn.com>
2018-01-09 20:57:16 -0500

    Create query handlers from connections

--- 31/31.03-fingerd/src/Main.hs
+++ 31/31.03-fingerd/src/Main.hs
@@ -105,7 +105,7 @@ returnUsers :: Connection -> Socket -> IO ()
 returnUsers dbConn soc = do
   rows <- query_ dbConn allUsers
   let usernames       = map username rows
-      newlinSeparated = T.concat $ intersperse "\n" usernames
+      newlineSeparated = T.concat $ intersperse "\n" usernames
   sendAll soc (encodeUtf8 newlineSeparated)
 
 formatUser :: User -> ByteString
@@ -127,5 +127,31 @@ returnUser dbConn soc username = do
       return ()
     Just user -> sendAll soc (formatUser user)
 
+handleQuery :: Connection -> Socket -> IO ()
+handleQuery dbConn soc = do
+  msg <- recv soc 1024
+  case msg of
+    "\r\n" -> returnUsers dbConn soc
+    name   -> returnUser dbConn soc (decodeUtf8 name)
+
+handleQueries :: Connection -> Socket -> IO ()
+handleQueries dbConn sock = forever $ do
+  (soc, _) <- accept sock
+  putStrLn "Got connection, handling query"
+  handleQuery dbConn soc
+  sClose soc
+
+
 main :: IO ()
-main = createDatabase
+main = withSocketsDo $ do
+  addrinfos <- getAddrInfo
+               (Just (defaultHints { addrFlags = [AI_PASSIVE] }))
+               Nothing (Just "79")
+  let serveraddr = head addrinfos
+  sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+  bindSocket sock (addrAddress serveraddr)
+  listen sock 1
+  conn <- open "finger.db"
+  handleQueries conn sock
+  SQLite.close conn
+  sClose sock
