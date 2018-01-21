d89591fe76e0198b82910fb5a6e4a889f0ee86a1
Evan Turner <ev@evturn.com>
2018-01-21 04:36:43 -0500

    Add example of dispatching type class instances by type

---
+++ 06/06.12-numberish.hs
@@ -0,0 +1,27 @@
+class Numberish a where
+  fromNumber    :: Integer -> a
+  toNumber      :: a -> Integer
+  defaultNumber :: a
+
+newtype Age = Age Integer
+  deriving (Eq, Show)
+
+instance Numberish Age where
+  fromNumber n     = Age n
+  toNumber (Age n) = n
+  defaultNumber    = Age 65
+
+newtype Year = Year Integer
+  deriving (Eq, Show)
+
+instance Numberish Year where
+  fromNumber n      = Year n
+  toNumber (Year n) = n
+  defaultNumber     = Year 1988
+
+sumNumberish :: Numberish a => a -> a -> a
+sumNumberish a a' = fromNumber summed
+  where
+    integerOfA      = toNumber a
+    integerOfAPrime = toNumber a'
+    summed          = integerOfA + integerOfAPrime
