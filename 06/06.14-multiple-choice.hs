5f8fec157e6667c2dcd0bb260d054b603c2e912b
Evan Turner <ev@evturn.com>
2018-01-21 05:11:20 -0500

    Add solutions to multiple choice chapter exercises

---
+++ 06/06.14-multiple-choice.hs
@@ -0,0 +1,28 @@
+-- 1.
+-- The Eq class
+-- Answer:
+-- c) makes equality tests possible
+
+-----------------------------------------------------------------------------
+-- 2.
+-- The typeclass Ord
+-- Answer:
+-- b) is a subclass of Eq
+
+-----------------------------------------------------------------------------
+-- 3.
+-- What is the type of `>`?
+-- Answer:
+-- a) Ord a => a -> a -> Bool
+
+-----------------------------------------------------------------------------
+-- 4.
+-- In `x = divMod 16 12`
+-- Answer:
+-- c) the type of x is a tuple
+
+-----------------------------------------------------------------------------
+-- 5.
+-- The typeclass Integal includes
+-- Answer:
+-- a) Int and Integer numbers
